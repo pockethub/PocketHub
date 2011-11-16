0913a530ba5a1c16943ab66c17df5ffda8c79e69
Kevin Sawicki <kevinsawicki@gmail.com>
2011-11-16 13:04:09 -0800

    Add fragment for displaying Gists

--- github-android/src/main/java/com/github/mobile/android/DashboardActivity.java
+++ github-android/src/main/java/com/github/mobile/android/DashboardActivity.java
@@ -4,6 +4,8 @@ package com.github.mobile.android;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.widget.TabHost;
+
+import com.github.mobile.android.gist.GistFragment;
 import com.github.mobile.android.ui.fragments.IssuesFragment;
 import com.github.mobile.android.ui.fragments.PullRequestsFragment;
 import com.github.mobile.android.ui.fragments.TabsAdapter;
@@ -11,6 +13,7 @@ import roboguice.activity.RoboFragmentActivity;
 
 import static com.github.mobile.android.R.string.issues;
 import static com.github.mobile.android.R.string.pull_requests;
+import static com.github.mobile.android.R.string.gists;
 
 public class DashboardActivity extends RoboFragmentActivity {
 	public static final String BUNDLE_KEY_TAB = "tab";
@@ -34,6 +37,7 @@ public class DashboardActivity extends RoboFragmentActivity {
 		// addTab("news", news, CountingFragment.class);
 		addTab("issues", issues, IssuesFragment.class);
 		addTab("pulls", pull_requests, PullRequestsFragment.class);
+		addTab("gists", gists, GistFragment.class);
 
         if (savedInstanceState != null) {
             tabHost.setCurrentTabByTag(savedInstanceState.getString(BUNDLE_KEY_TAB));
---
+++ github-android/src/main/java/com/github/mobile/android/gist/GistViewHolder.java
@@ -0,0 +1,32 @@
+package com.github.mobile.android.gist;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.mobile.android.R;
+import com.madgag.android.listviews.ViewHolder;
+
+import org.eclipse.egit.github.core.Gist;
+
+/**
+ * View holder for a {@link Gist}
+ */
+public class GistViewHolder implements ViewHolder<Gist> {
+
+	private final TextView title;
+
+	/**
+	 * Create view holder for a {@link Gist}
+	 * 
+	 * @param v
+	 */
+	public GistViewHolder(View v) {
+		title = (TextView) v.findViewById(R.id.gv_list_item_id);
+	}
+
+	@Override
+	public void updateViewFor(Gist gist) {
+		String id = gist.getId();
+		title.setText(id);
+	}
+}
