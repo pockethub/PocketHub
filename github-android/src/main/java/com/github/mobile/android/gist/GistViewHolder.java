4346e45737fd7f4a97dddc13e96fab2232ea9103
Kevin Sawicki <kevinsawicki@gmail.com>
2011-12-02 10:38:45 -0800

    Show more information on listed Gists

--- github-android/src/main/java/com/github/mobile/android/gist/GistViewHolder.java
+++ github-android/src/main/java/com/github/mobile/android/gist/GistViewHolder.java
@@ -3,7 +3,9 @@ package com.github.mobile.android.gist;
 import android.view.View;
 import android.widget.TextView;
 
-import com.github.mobile.android.R;
+import com.github.mobile.android.R.id;
+import com.github.mobile.android.R.string;
+import com.github.mobile.android.util.Time;
 import com.madgag.android.listviews.ViewHolder;
 
 import org.eclipse.egit.github.core.Gist;
@@ -13,7 +15,15 @@ import org.eclipse.egit.github.core.Gist;
  */
 public class GistViewHolder implements ViewHolder<Gist> {
 
+    private final TextView gistId;
+
     private final TextView title;
+
+    private final TextView created;
+
+    private final TextView comments;
+
+    private final TextView files;
 
     /**
      * Create view holder for a {@link Gist}
@@ -21,12 +31,31 @@ public class GistViewHolder implements ViewHolder<Gist> {
      * @param v
      */
     public GistViewHolder(View v) {
-        title = (TextView) v.findViewById(R.id.gv_list_item_id);
+        gistId = (TextView) v.findViewById(id.tv_gist_id);
+        title = (TextView) v.findViewById(id.tv_gist_title);
+        created = (TextView) v.findViewById(id.tv_gist_creation);
+        comments = (TextView) v.findViewById(id.tv_gist_comments);
+        files = (TextView) v.findViewById(id.tv_gist_files);
     }
 
     @Override
-    public void updateViewFor(Gist gist) {
+    public void updateViewFor(final Gist gist) {
         String id = gist.getId();
-        title.setText(id);
+        if (!gist.isPublic() && id.length() > 8)
+            id = id.substring(0, 8) + "...";
+        gistId.setText(id);
+
+        title.setText(gist.getDescription());
+        created.setText(Time.relativeTimeFor(gist.getCreatedAt()));
+
+        if (gist.getComments() != 1)
+            comments.setText(comments.getContext().getString(string.multiple_comments, gist.getComments()));
+        else
+            comments.setText(string.single_comment);
+
+        if (gist.getFiles().size() != 1)
+            files.setText(files.getContext().getString(string.multiple_files, gist.getFiles().size()));
+        else
+            files.setText(string.single_file);
     }
 }
