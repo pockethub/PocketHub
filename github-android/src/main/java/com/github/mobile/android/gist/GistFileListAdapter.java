a42b9b3c6c73322e771fea0590e618d4ea8e6ec9
Kevin Sawicki <kevin@github.com>
2011-12-07 18:03:17 -0800

    Indent Gist file name with '»' character

--- github-android/src/main/java/com/github/mobile/android/gist/GistFileListAdapter.java
+++ github-android/src/main/java/com/github/mobile/android/gist/GistFileListAdapter.java
@@ -6,7 +6,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.github.mobile.android.R;
+import com.github.mobile.android.R.layout;
 
 import org.eclipse.egit.github.core.GistFile;
 
@@ -24,15 +24,14 @@ public class GistFileListAdapter extends ArrayAdapter<GistFile> {
      * @param files
      */
     public GistFileListAdapter(Activity activity, GistFile[] files) {
-        super(activity, R.layout.gist_view_file_item, files);
+        super(activity, layout.gist_view_file_item, files);
         this.activity = activity;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        final GistFile file = getItem(position);
-        final TextView view = (TextView) activity.getLayoutInflater().inflate(R.layout.gist_view_file_item, null);
-        view.setText(file.getFilename());
+        final TextView view = (TextView) activity.getLayoutInflater().inflate(layout.gist_view_file_item, null);
+        view.setText("»   " + getItem(position).getFilename());
         return view;
     }
 }
