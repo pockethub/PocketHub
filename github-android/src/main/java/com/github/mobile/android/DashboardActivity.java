20a1f1c21e5a4bff8982446cb3376da0fb6ac592
Kevin Sawicki <kevin@github.com>
2011-12-09 14:10:07 -0800

    Experiment with using custom tabs
    Although the tab widget looks good by default on ICS,
    it looks pretty unpleasant on Gingerbread.
    
    This commit adds custom tab layouts that will look the
    same on all versions.

--- github-android/src/main/java/com/github/mobile/android/DashboardActivity.java
+++ github-android/src/main/java/com/github/mobile/android/DashboardActivity.java
@@ -7,8 +7,12 @@ import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
+import com.github.mobile.android.R.id;
 import com.github.mobile.android.gist.GistFragment;
 import com.github.mobile.android.ui.WelcomeActivity;
 import com.github.mobile.android.ui.fragments.IssuesFragment;
@@ -40,7 +44,7 @@ public class DashboardActivity extends RoboFragmentActivity {
         viewPager = (ViewPager) findViewById(R.id.pager);
 
         tabsAdapter = new TabsAdapter(this, tabHost, viewPager);
-
+        tabHost.getTabWidget().setDividerDrawable(null);
         addTab("issues", issues, IssuesFragment.class);
         addTab("pulls", pull_requests, PullRequestsFragment.class);
         addTab("gists", gists, GistFragment.class);
@@ -59,7 +63,12 @@ public class DashboardActivity extends RoboFragmentActivity {
     }
 
     private void addTab(String tag, int indicator, Class<?> clazz) {
-        tabsAdapter.addTab(tabHost.newTabSpec(tag).setIndicator(this.getResources().getString(indicator)), clazz, null);
+        TabSpec spec = tabHost.newTabSpec(tag);
+        View view = getLayoutInflater().inflate(R.layout.tab, null);
+        spec.setIndicator(view);
+        TextView text = (TextView) view.findViewById(id.tv_tab);
+        text.setText(getResources().getString(indicator));
+        tabsAdapter.addTab(spec, clazz, null);
     }
 
     @Override
