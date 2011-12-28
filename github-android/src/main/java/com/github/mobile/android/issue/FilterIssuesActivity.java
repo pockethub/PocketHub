1751645e74b40ea8add1252c58c2adef1aecf77a
Kevin Sawicki <kevin@github.com>
2011-12-28 12:01:36 -0800

    Cache loaded milestones, labels, and assignee
    Also support clearing the value of each filter field

--- github-android/src/main/java/com/github/mobile/android/issue/FilterIssuesActivity.java
+++ github-android/src/main/java/com/github/mobile/android/issue/FilterIssuesActivity.java
@@ -11,14 +11,15 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.github.mobile.android.R;
+import com.github.mobile.android.R.id;
+import com.github.mobile.android.R.layout;
 import com.github.mobile.android.util.GitHubIntents;
 import com.google.inject.Inject;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -49,6 +50,18 @@ public class FilterIssuesActivity extends RoboActivity {
     @Inject
     private LabelService labels;
 
+    private List<Label> allLabels;
+
+    private List<User> allCollaborators;
+
+    private List<Milestone> allMilestones;
+
+    private Set<String> selectedLabels;
+
+    private String selectedAssignee;
+
+    private int selectedMilestone = -1;
+
     /**
      * Create intent for creating an issue filter for the given repository
      *
@@ -62,158 +75,125 @@ public class FilterIssuesActivity extends RoboActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.issues_filter);
+        setContentView(layout.issues_filter);
 
         final Repository repository = (Repository) getIntent().getSerializableExtra(GitHubIntents.EXTRA_REPOSITORY);
 
         OnClickListener assigneeListener = new OnClickListener() {
 
             public void onClick(View v) {
-                final ProgressDialog loader = new ProgressDialog(FilterIssuesActivity.this);
-                loader.setMessage("Loading Collaborators");
-                loader.show();
-                new RoboAsyncTask<List<User>>(FilterIssuesActivity.this) {
+                if (allCollaborators == null) {
+                    final ProgressDialog loader = new ProgressDialog(FilterIssuesActivity.this);
+                    loader.setMessage("Loading Users..");
+                    loader.show();
+                    new RoboAsyncTask<List<User>>(FilterIssuesActivity.this) {
 
-                    public List<User> call() throws Exception {
-                        return collaborators.getCollaborators(repository);
-                    }
+                        public List<User> call() throws Exception {
+                            allCollaborators = collaborators.getCollaborators(repository);
+                            Collections.sort(allCollaborators, new Comparator<User>() {
 
-                    protected void onSuccess(List<User> users) throws Exception {
-                        loader.dismiss();
-                        final Builder prompt = new Builder(FilterIssuesActivity.this);
-                        prompt.setTitle("Select An Assignee:");
-                        final String[] logins = new String[users.size()];
-                        for (int i = 0; i < logins.length; i++)
-                            logins[i] = users.get(i).getLogin();
-                        Arrays.sort(logins, new Comparator<String>() {
+                                public int compare(User u1, User u2) {
+                                    return u1.getLogin().compareToIgnoreCase(u2.getLogin());
+                                }
+                            });
+                            return allCollaborators;
+                        }
 
-                            public int compare(String s1, String s2) {
-                                return s1.compareToIgnoreCase(s2);
-                            }
-                        });
-                        prompt.setSingleChoiceItems(logins, -1, new DialogInterface.OnClickListener() {
+                        protected void onSuccess(List<User> users) throws Exception {
+                            loader.dismiss();
+                            promptForAssignee();
+                        }
 
-                            public void onClick(DialogInterface dialog, int which) {
-                                ((TextView) findViewById(R.id.tv_assignee)).setText(logins[which]);
-                                dialog.dismiss();
-                            }
-                        });
-                        prompt.show();
-                    }
-                }.execute();
+                        protected void onException(Exception e) throws RuntimeException {
+                            loader.dismiss();
+                        }
+                    }.execute();
+                } else
+                    promptForAssignee();
             }
         };
 
-        ((TextView) findViewById(R.id.tv_assignee_label)).setOnClickListener(assigneeListener);
-        ((TextView) findViewById(R.id.tv_assignee)).setOnClickListener(assigneeListener);
+        ((TextView) findViewById(id.tv_assignee_label)).setOnClickListener(assigneeListener);
+        ((TextView) findViewById(id.tv_assignee)).setOnClickListener(assigneeListener);
 
         OnClickListener milestoneListener = new OnClickListener() {
 
             public void onClick(View v) {
-                final ProgressDialog loader = new ProgressDialog(FilterIssuesActivity.this);
-                loader.setMessage("Loading Milestones");
-                loader.show();
-                new RoboAsyncTask<List<Milestone>>(FilterIssuesActivity.this) {
+                if (allMilestones == null) {
+                    final ProgressDialog loader = new ProgressDialog(FilterIssuesActivity.this);
+                    loader.setMessage("Loading Milestones...");
+                    loader.show();
+                    new RoboAsyncTask<List<Milestone>>(FilterIssuesActivity.this) {
 
-                    public List<Milestone> call() throws Exception {
-                        List<Milestone> all = new LinkedList<Milestone>();
-                        all.addAll(milestones.getMilestones(repository, IssueService.STATE_OPEN));
-                        all.addAll(milestones.getMilestones(repository, IssueService.STATE_CLOSED));
-                        return all;
-                    }
+                        public List<Milestone> call() throws Exception {
+                            List<Milestone> all = new ArrayList<Milestone>();
+                            all.addAll(milestones.getMilestones(repository, IssueService.STATE_OPEN));
+                            all.addAll(milestones.getMilestones(repository, IssueService.STATE_CLOSED));
+                            Collections.sort(all, new Comparator<Milestone>() {
 
-                    protected void onSuccess(List<Milestone> all) throws Exception {
-                        loader.dismiss();
-                        final Builder prompt = new Builder(FilterIssuesActivity.this);
-                        prompt.setTitle("Select A Milestone:");
-                        final String[] names = new String[all.size()];
-                        for (int i = 0; i < names.length; i++)
-                            names[i] = all.get(i).getTitle();
+                                public int compare(Milestone m1, Milestone m2) {
+                                    return m1.getTitle().compareToIgnoreCase(m2.getTitle());
+                                }
+                            });
+                            allMilestones = all;
+                            return allMilestones;
+                        }
 
-                        Arrays.sort(names, new Comparator<String>() {
+                        protected void onSuccess(List<Milestone> all) throws Exception {
+                            loader.dismiss();
+                            promptForMilestone();
+                        }
 
-                            public int compare(String s1, String s2) {
-                                return s1.compareToIgnoreCase(s2);
-                            }
-                        });
-                        prompt.setSingleChoiceItems(names, -1, new DialogInterface.OnClickListener() {
-
-                            public void onClick(DialogInterface dialog, int which) {
-                                ((TextView) findViewById(R.id.tv_milestone)).setText(names[which]);
-                                dialog.dismiss();
-                            }
-                        });
-                        prompt.show();
-                    }
-                }.execute();
+                        protected void onException(Exception e) throws RuntimeException {
+                            loader.dismiss();
+                        }
+                    }.execute();
+                } else
+                    promptForMilestone();
             }
         };
 
-        ((TextView) findViewById(R.id.tv_milestone_label)).setOnClickListener(milestoneListener);
-        ((TextView) findViewById(R.id.tv_milestone)).setOnClickListener(milestoneListener);
+        ((TextView) findViewById(id.tv_milestone_label)).setOnClickListener(milestoneListener);
+        ((TextView) findViewById(id.tv_milestone)).setOnClickListener(milestoneListener);
 
         OnClickListener labelsListener = new OnClickListener() {
 
             public void onClick(View v) {
-                final ProgressDialog loader = new ProgressDialog(FilterIssuesActivity.this);
-                loader.setMessage("Loading Labels");
-                loader.show();
-                new RoboAsyncTask<List<Label>>(FilterIssuesActivity.this) {
+                if (allLabels == null) {
+                    final ProgressDialog loader = new ProgressDialog(FilterIssuesActivity.this);
+                    loader.setMessage("Loading Labels...");
+                    loader.show();
+                    new RoboAsyncTask<List<Label>>(FilterIssuesActivity.this) {
 
-                    public List<Label> call() throws Exception {
-                        return labels.getLabels(repository);
-                    }
+                        public List<Label> call() throws Exception {
+                            allLabels = labels.getLabels(repository);
+                            Collections.sort(allLabels, new Comparator<Label>() {
 
-                    protected void onSuccess(List<Label> all) throws Exception {
-                        loader.dismiss();
-                        final Builder prompt = new Builder(FilterIssuesActivity.this);
-                        prompt.setTitle("Select Labels:");
-                        final String[] names = new String[all.size()];
-                        for (int i = 0; i < names.length; i++)
-                            names[i] = all.get(i).getName();
-
-                        Arrays.sort(names, new Comparator<String>() {
-
-                            public int compare(String s1, String s2) {
-                                return s1.compareToIgnoreCase(s2);
-                            }
-                        });
-
-                        final Set<String> selected = new TreeSet<String>();
-                        prompt.setMultiChoiceItems(names, null, new OnMultiChoiceClickListener() {
-
-                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                                if (isChecked)
-                                    selected.add(names[which]);
-                                else
-                                    selected.remove(names[which]);
-                            }
-                        });
-                        prompt.setPositiveButton("Apply", new DialogInterface.OnClickListener() {
-
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                if (selected.size() == 1)
-                                    ((TextView) findViewById(R.id.tv_labels)).setText(selected.iterator().next());
-                                else if (!selected.isEmpty()) {
-                                    StringBuilder selectedLabels = new StringBuilder();
-                                    for (String label : selected)
-                                        selectedLabels.append(label).append(',').append(' ');
-                                    ((TextView) findViewById(R.id.tv_labels)).setText(selectedLabels.substring(0,
-                                            selectedLabels.length() - 2));
+                                public int compare(Label l1, Label l2) {
+                                    return l1.getName().compareToIgnoreCase(l2.getName());
                                 }
-                            }
-                        });
-                        prompt.show();
-                    }
-                }.execute();
+                            });
+                            return allLabels;
+                        }
+
+                        protected void onSuccess(List<Label> all) throws Exception {
+                            loader.dismiss();
+                            promptForLabels();
+                        }
+
+                        protected void onException(Exception e) throws RuntimeException {
+                            loader.dismiss();
+                        }
+                    }.execute();
+                } else
+                    promptForLabels();
             }
         };
 
-        ((TextView) findViewById(R.id.tv_labels_label)).setOnClickListener(labelsListener);
-        ((TextView) findViewById(R.id.tv_labels)).setOnClickListener(labelsListener);
+        ((TextView) findViewById(id.tv_labels_label)).setOnClickListener(labelsListener);
+        ((TextView) findViewById(id.tv_labels)).setOnClickListener(labelsListener);
 
-        ((Button) findViewById(R.id.b_apply)).setOnClickListener(new OnClickListener() {
+        ((Button) findViewById(id.b_apply)).setOnClickListener(new OnClickListener() {
 
             public void onClick(View v) {
                 HashMap<String, String> filter = new HashMap<String, String>();
@@ -224,5 +204,118 @@ public class FilterIssuesActivity extends RoboActivity {
                 finish();
             }
         });
+    }
+
+    private void promptForLabels() {
+        final Builder prompt = new Builder(FilterIssuesActivity.this);
+        prompt.setTitle("Select Labels:");
+        final String[] names = new String[allLabels.size()];
+        final boolean[] checked = new boolean[names.length];
+        if (selectedLabels == null)
+            for (int i = 0; i < names.length; i++)
+                names[i] = allLabels.get(i).getName();
+        else
+            for (int i = 0; i < names.length; i++) {
+                names[i] = allLabels.get(i).getName();
+                if (selectedLabels.contains(names[i]))
+                    checked[i] = true;
+            }
+
+        final Set<String> selected = new TreeSet<String>();
+        prompt.setMultiChoiceItems(names, checked, new OnMultiChoiceClickListener() {
+
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked)
+                    selected.add(names[which]);
+                else
+                    selected.remove(names[which]);
+            }
+        });
+        prompt.setPositiveButton("Apply", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                selectedLabels = selected;
+                if (selected.size() == 1)
+                    ((TextView) findViewById(id.tv_labels)).setText(selected.iterator().next());
+                else if (!selected.isEmpty()) {
+                    StringBuilder labelsLabel = new StringBuilder();
+                    for (String label : selected)
+                        labelsLabel.append(label).append(',').append(' ');
+                    ((TextView) findViewById(id.tv_labels)).setText(labelsLabel.substring(0, labelsLabel.length() - 2));
+                }
+            }
+        });
+        prompt.setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                selectedLabels = null;
+                ((TextView) findViewById(id.tv_labels)).setText("");
+                dialog.dismiss();
+            }
+        });
+        prompt.show();
+    }
+
+    private void promptForMilestone() {
+        final Builder prompt = new Builder(FilterIssuesActivity.this);
+        prompt.setTitle("Select A Milestone:");
+
+        final String[] names = new String[allMilestones.size()];
+        int selected = -1;
+        for (int i = 0; i < names.length; i++) {
+            names[i] = allMilestones.get(i).getTitle();
+            if (selectedMilestone == allMilestones.get(i).getNumber())
+                selected = i;
+        }
+
+        prompt.setSingleChoiceItems(names, selected, new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                selectedMilestone = allMilestones.get(which).getNumber();
+                ((TextView) findViewById(id.tv_milestone)).setText(names[which]);
+                dialog.dismiss();
+            }
+        });
+        prompt.setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                selectedMilestone = -1;
+                ((TextView) findViewById(id.tv_milestone)).setText("");
+                dialog.dismiss();
+            }
+        });
+        prompt.show();
+    }
+
+    private void promptForAssignee() {
+        final Builder prompt = new Builder(FilterIssuesActivity.this);
+        prompt.setTitle("Select An Assignee:");
+
+        final String[] logins = new String[allCollaborators.size()];
+        int selected = -1;
+        for (int i = 0; i < logins.length; i++) {
+            logins[i] = allCollaborators.get(i).getLogin();
+            if (logins[i].equals(selectedAssignee))
+                selected = i;
+        }
+
+        prompt.setSingleChoiceItems(logins, selected, new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                selectedAssignee = logins[which];
+                ((TextView) findViewById(id.tv_assignee)).setText(selectedAssignee);
+                dialog.dismiss();
+            }
+        });
+        prompt.setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                selectedAssignee = null;
+                ((TextView) findViewById(id.tv_assignee)).setText("");
+                dialog.dismiss();
+            }
+        });
+        prompt.show();
     }
 }
