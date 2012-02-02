66044dde19f0140bb1fa61edea5e98520ea89e3d
Kevin Sawicki <kevin@github.com>
2012-02-02 10:11:05 -0800

    Use custom title in IssueDashboardActivity

--- github-android/src/main/java/com/github/mobile/android/issue/IssueDashboardActivity.java
+++ github-android/src/main/java/com/github/mobile/android/issue/IssueDashboardActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.view.ViewPager;
 
 import com.github.mobile.android.R.id;
 import com.github.mobile.android.R.layout;
+import com.github.mobile.android.R.string;
 import com.viewpagerindicator.TitlePageIndicator;
 
 import roboguice.activity.RoboFragmentActivity;
@@ -25,6 +26,8 @@ public class IssueDashboardActivity extends RoboFragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.issue_dashboard);
+
+        setTitle(getString(string.dashboard_issues_title));
 
         pager.setAdapter(new IssueDashboardPagerAdapter(getApplicationContext(), getSupportFragmentManager()));
         indicator.setViewPager(pager);
