3febc8f81e83c7ef3df2cb711170a89071ec46d4
Kevin Sawicki <kevin@github.com>
2012-02-08 09:05:20 -0800

    Show toast when loading orgs fails

--- github-android/src/main/java/com/github/mobile/android/repo/OrgListFragment.java
+++ github-android/src/main/java/com/github/mobile/android/repo/OrgListFragment.java
@@ -1,15 +1,18 @@
 package com.github.mobile.android.repo;
 
+import static android.util.Log.DEBUG;
 import android.os.Bundle;
 import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.github.mobile.android.AccountDataManager;
 import com.github.mobile.android.AsyncLoader;
 import com.github.mobile.android.R.layout;
+import com.github.mobile.android.R.string;
 import com.github.mobile.android.ui.fragments.ListLoadingFragment;
 import com.google.inject.Inject;
 import com.madgag.android.listviews.ReflectiveHolderFactory;
@@ -27,7 +30,7 @@ import org.eclipse.egit.github.core.client.GitHubClient;
 /**
  * Fragment to load a list of GitHub organizations
  */
-public class OrgListFragment extends ListLoadingFragment<User> {
+public class OrgListFragment extends ListLoadingFragment<User> implements Comparator<User> {
 
     private static final String TAG = "OLF";
 
@@ -37,31 +40,32 @@ public class OrgListFragment extends ListLoadingFragment<User> {
     @Inject
     private GitHubClient client;
 
+    @Override
     public Loader<List<User>> onCreateLoader(int id, Bundle args) {
         return new AsyncLoader<List<User>>(getActivity()) {
 
             public List<User> loadInBackground() {
                 try {
                     List<User> orgs = cache.getOrgs();
-                    Collections.sort(orgs, new Comparator<User>() {
-
-                        public int compare(User lhs, User rhs) {
-                            if (lhs.getLogin().equals(client.getUser()))
-                                return -1;
-                            if (rhs.getLogin().equals(client.getUser()))
-                                return 1;
-                            return lhs.getLogin().compareToIgnoreCase(rhs.getLogin());
-                        }
-                    });
+                    Collections.sort(orgs, OrgListFragment.this);
                     return orgs;
                 } catch (IOException e) {
-                    Log.d(TAG, "Exception loading organizations", e);
+                    if (Log.isLoggable(TAG, DEBUG))
+                        Log.d(TAG, "Exception loading organizations", e);
+
+                    getActivity().runOnUiThread(new Runnable() {
+
+                        public void run() {
+                            Toast.makeText(getContext(), getActivity().getString(string.error_orgs_load), 5000).show();
+                        }
+                    });
+                    return Collections.emptyList();
                 }
-                return Collections.emptyList();
             }
         };
     }
 
+    @Override
     protected ListAdapter adapterFor(List<User> items) {
         return new ViewHoldingListAdapter<User>(items, ViewInflator.viewInflatorFor(getActivity(), layout.org_item),
                 ReflectiveHolderFactory.reflectiveFactoryFor(OrgViewHolder.class, getActivity()));
@@ -71,5 +75,15 @@ public class OrgListFragment extends ListLoadingFragment<User> {
     public void onListItemClick(ListView l, View v, int position, long id) {
         User user = (User) l.getItemAtPosition(position);
         startActivity(RepoBrowseActivity.createIntent(user));
+    }
+
+    @Override
+    public int compare(final User lhs, final User rhs) {
+        if (lhs.getLogin().equals(client.getUser()))
+            return -1;
+        if (rhs.getLogin().equals(client.getUser()))
+            return 1;
+
+        return lhs.getLogin().compareToIgnoreCase(rhs.getLogin());
     }
 }
