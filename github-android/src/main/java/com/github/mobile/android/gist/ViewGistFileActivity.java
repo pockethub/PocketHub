20a540fae93eac24c6141fa67d0f0f55445d9454
Kevin Sawicki <kevin@github.com>
2012-03-14 13:49:25 -0700

    Add home link to ViewGistFileActivity
    Selecting the home link returns to the Gist view

--- github-android/src/main/java/com/github/mobile/android/gist/ViewGistFileActivity.java
+++ github-android/src/main/java/com/github/mobile/android/gist/ViewGistFileActivity.java
@@ -1,5 +1,7 @@
 package com.github.mobile.android.gist;
 
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
 import static com.github.mobile.android.util.GitHubIntents.EXTRA_GIST;
 import static com.github.mobile.android.util.GitHubIntents.EXTRA_GIST_FILE;
 import android.content.Intent;
@@ -7,22 +9,24 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.widget.TextView;
 
+import com.actionbarsherlock.view.MenuItem;
 import com.github.mobile.android.R.id;
 import com.github.mobile.android.R.layout;
 import com.github.mobile.android.R.string;
 import com.github.mobile.android.util.GitHubIntents.Builder;
 import com.github.mobile.android.util.SourceEditor;
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockFragmentActivity;
 
 import org.eclipse.egit.github.core.Gist;
 import org.eclipse.egit.github.core.GistFile;
 
-import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectExtra;
 import roboguice.inject.InjectView;
 
 /**
  * Activity to view a file in a Gist
  */
-public class ViewGistFileActivity extends RoboActivity {
+public class ViewGistFileActivity extends RoboSherlockFragmentActivity {
 
     /**
      * Create intent to show file
@@ -41,14 +45,18 @@ public class ViewGistFileActivity extends RoboActivity {
     @InjectView(id.wv_gist_content)
     private WebView webView;
 
+    @InjectExtra(EXTRA_GIST)
+    private Gist gist;
+
+    @InjectExtra(EXTRA_GIST_FILE)
+    private GistFile file;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.gist_view_content_item);
-
-        final Gist gist = (Gist) getIntent().getSerializableExtra(EXTRA_GIST);
-        final GistFile file = (GistFile) getIntent().getSerializableExtra(EXTRA_GIST_FILE);
-
         setTitle(file.getFilename());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         gistId.setText(getString(string.gist) + " " + gist.getId());
 
         SourceEditor.showSource(webView, file.getFilename(), new Object() {
@@ -56,5 +64,19 @@ public class ViewGistFileActivity extends RoboActivity {
                 return file.getContent();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            Intent intent = new Intent(this, ViewGistActivity.class);
+            intent.putExtra(EXTRA_GIST, gist);
+            intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
