65e2532948cd1b4b39a15b08ddd3d159a21de106
Kevin Sawicki <kevin@github.com>
2012-03-14 16:05:06 -0700

    Load gist file is activity created before content is available
    This can occur if a Gist is selected from a list view and then
    a file is immediately selected before the Gist file contents
    load.

--- github-android/src/main/java/com/github/mobile/android/gist/ViewGistFileActivity.java
+++ github-android/src/main/java/com/github/mobile/android/gist/ViewGistFileActivity.java
@@ -2,8 +2,8 @@ package com.github.mobile.android.gist;
 
 import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
 import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
-import static com.github.mobile.android.util.GitHubIntents.EXTRA_GIST;
 import static com.github.mobile.android.util.GitHubIntents.EXTRA_GIST_FILE;
+import static com.github.mobile.android.util.GitHubIntents.EXTRA_GIST_ID;
 import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
@@ -13,15 +13,21 @@ import com.actionbarsherlock.view.MenuItem;
 import com.github.mobile.android.R.id;
 import com.github.mobile.android.R.layout;
 import com.github.mobile.android.R.string;
+import com.github.mobile.android.util.ErrorHelper;
 import com.github.mobile.android.util.GitHubIntents.Builder;
 import com.github.mobile.android.util.SourceEditor;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockFragmentActivity;
+import com.google.inject.Inject;
+
+import java.io.IOException;
+import java.util.Map;
 
 import org.eclipse.egit.github.core.Gist;
 import org.eclipse.egit.github.core.GistFile;
 
 import roboguice.inject.InjectExtra;
 import roboguice.inject.InjectView;
+import roboguice.util.RoboAsyncTask;
 
 /**
  * Activity to view a file in a Gist
@@ -36,20 +42,25 @@ public class ViewGistFileActivity extends RoboSherlockFragmentActivity {
      * @return intent
      */
     public static Intent createIntent(Gist gist, GistFile file) {
-        return new Builder("gist.file.VIEW").add(EXTRA_GIST, gist).add(EXTRA_GIST_FILE, file).toIntent();
+        return new Builder("gist.file.VIEW").add(EXTRA_GIST_ID, gist.getId()).add(EXTRA_GIST_FILE, file).toIntent();
     }
 
     @InjectView(id.tv_gist_id)
-    private TextView gistId;
+    private TextView idText;
 
     @InjectView(id.wv_gist_content)
     private WebView webView;
 
-    @InjectExtra(EXTRA_GIST)
-    private Gist gist;
+    @InjectExtra(EXTRA_GIST_ID)
+    private String gistId;
 
     @InjectExtra(EXTRA_GIST_FILE)
     private GistFile file;
+
+    private Gist gist;
+
+    @Inject
+    private GistStore store;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,8 +68,49 @@ public class ViewGistFileActivity extends RoboSherlockFragmentActivity {
         setTitle(file.getFilename());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        gistId.setText(getString(string.gist) + " " + gist.getId());
+        idText.setText(getString(string.gist) + " " + gistId);
+        gist = store.getGist(gistId);
+        if (gist == null)
+            gist = new Gist().setId(gistId);
 
+        if (file.getContent() != null)
+            showSource();
+        else
+            loadSource();
+
+    }
+
+    private void loadSource() {
+        new RoboAsyncTask<GistFile>(this) {
+            public GistFile call() throws Exception {
+                gist = store.refreshGist(gistId);
+                Map<String, GistFile> files = gist.getFiles();
+                if (files == null)
+                    throw new IOException();
+                GistFile loadedFile = files.get(file.getFilename());
+                if (loadedFile == null)
+                    throw new IOException();
+                return loadedFile;
+            }
+
+            protected void onException(Exception e) throws RuntimeException {
+                ErrorHelper.show(getApplication(), e, string.error_gist_file_load);
+            }
+
+            protected void onSuccess(GistFile loadedFile) throws Exception {
+                if (loadedFile == null)
+                    return;
+
+                file = loadedFile;
+                getIntent().putExtra(EXTRA_GIST_FILE, file);
+                if (file.getContent() != null)
+                    showSource();
+            }
+
+        }.execute();
+    }
+
+    private void showSource() {
         SourceEditor.showSource(webView, file.getFilename(), new Object() {
             public String toString() {
                 return file.getContent();
