4b8391b89d7dd4f573364480e9237743710a59fc
Kevin Sawicki <kevin@github.com>
2012-04-16 14:07:46 -0700

    Add support for opening gists from event selections

--- app/src/main/java/com/github/mobile/android/ui/NewsFragment.java
+++ app/src/main/java/com/github/mobile/android/ui/NewsFragment.java
@@ -3,9 +3,12 @@ package com.github.mobile.android.ui;
 import android.view.View;
 import android.widget.ListView;
 
+import com.github.mobile.android.core.gist.GistEventMatcher;
 import com.github.mobile.android.core.issue.IssueEventMatcher;
+import com.github.mobile.android.gist.ViewGistsActivity;
 import com.github.mobile.android.issue.ViewIssueActivity;
 
+import org.eclipse.egit.github.core.Gist;
 import org.eclipse.egit.github.core.Issue;
 import org.eclipse.egit.github.core.event.Event;
 
@@ -19,11 +22,20 @@ public abstract class NewsFragment extends PagedListFragment<Event> {
      */
     protected final IssueEventMatcher issueMatcher = new IssueEventMatcher();
 
+    /**
+     * Matcher for finding a {@link Gist} from an {@link Event}
+     */
+    protected final GistEventMatcher gistMatcher = new GistEventMatcher();
+
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         Event event = (Event) l.getItemAtPosition(position);
         Issue issue = issueMatcher.getIssue(event);
         if (issue != null)
             startActivity(ViewIssueActivity.createIntent(issue));
+
+        Gist gist = gistMatcher.getGist(event);
+        if (gist != null)
+            startActivity(ViewGistsActivity.createIntent(gist));
     }
 }
