7185f8dbf50420cf736510cbee4128531e9f86a3
Kevin Sawicki <kevin@github.com>
2012-04-18 08:25:18 -0700

    Use static imports for constants

--- app/src/main/java/com/github/mobile/android/util/SharedPreferencesUtil.java
+++ app/src/main/java/com/github/mobile/android/util/SharedPreferencesUtil.java
@@ -1,21 +1,22 @@
 package com.github.mobile.android.util;
 
-
+import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.GINGERBREAD;
-import android.content.SharedPreferences;
-import android.os.Build;
+import android.content.SharedPreferences.Editor;
 
 public class SharedPreferencesUtil {
 
     private static boolean isEditorApplyAvailable() {
-        return Build.VERSION.SDK_INT >= GINGERBREAD;
+        return SDK_INT >= GINGERBREAD;
     }
 
     /**
      * Uses the non-blocking apply() method on post-Froyo devices, otherwise
      * use the older commit() method which blocks while doing IO.
+     *
+     * @param editor
      */
-    public static void savePrefsFrom(SharedPreferences.Editor editor) {
+    public static void savePrefsFrom(Editor editor) {
         if (isEditorApplyAvailable()) {
             editor.apply();
         } else {
