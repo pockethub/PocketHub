pockethub/PocketHub

commit 6708c75e4c3191e934636fd5dc962b89a244ae23

Include all visited repos in recent repo-history
Previously other routes to the repo-viewer page (other than clicking on
on a repo in the RepoListFragment) wouldn't be recorded as a repo visit.

app/src/main/java/com/github/mobile/android/repo/RecentReposHelper.java
@@ -1,25 +1,27 @@
 package com.github.mobile.android.repo;
 
+import static com.github.mobile.android.util.SharedPreferencesUtil.savePrefsFrom;
 import static com.google.common.base.Predicates.in;
 import static com.google.common.collect.Iterables.concat;
 import static com.google.common.collect.Iterables.filter;
 import static com.google.common.collect.Iterables.limit;
 import static com.google.common.collect.Iterables.transform;
 import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.newLinkedList;
 import static com.google.common.collect.Maps.uniqueIndex;
 import static com.google.common.collect.Sets.difference;
 import static com.google.common.collect.Sets.newLinkedHashSet;
 import static java.lang.String.CASE_INSENSITIVE_ORDER;
-import android.content.Context;
+import android.content.SharedPreferences;
 import android.text.TextUtils;
 
-import com.github.mobile.android.RequestReader;
-import com.github.mobile.android.RequestWriter;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Ordering;
+import com.google.inject.Inject;
 
-import java.io.File;
 import java.io.Serializable;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
@@ -35,14 +37,16 @@ import org.eclipse.egit.github.core.Repository;
  */
 public class RecentReposHelper {
 
-    private static final String FILE_RECENT_REPOS = "recent_repos.ser";
-
     /**
      * The maximum number of recent repos to store - this is the total across all different orgs.
      */
     private static final int MAX_RECENT_REPOS = 20;
 
-    private static final int VERSION_RECENT_REPOS = 2;
+    private static final String PREF_RECENT_REPOS = "recentRepos";
+
+    private static final Joiner JOINER = Joiner.on(",");
+
+    private static final Splitter SPLITTER = Splitter.on(",");
 
     private static final Function<Repository, String> REPO_NAME = new Function<Repository, String>() {
         public String apply(Repository repo) {
@@ -58,77 +62,42 @@ public class RecentReposHelper {
 
     private static Ordering<Repository> REPO_NAME_ORDER = Ordering.from(CASE_INSENSITIVE_ORDER).onResultOf(REPO_NAME);
 
-    private final LinkedList<String> recentRepos;
+    private LinkedList<String> recentRepos;
 
-    private final Context context;
+    @Inject
+    private SharedPreferences sharedPreferences;
 
     /**
-     * Create helper scoped to given user
-     *
-     * @param context
+     * Load recent repos from store.
      */
-    public RecentReposHelper(final Context context) {
-        this.context = context;
-
-        LinkedList<String> loaded = new RequestReader(getRecentReposFile(), VERSION_RECENT_REPOS).read();
-        if (loaded == null)
-            loaded = new LinkedList<String>();
-        recentRepos = loaded;
-        trimRecentRepos();
+    public void load() {
+        recentRepos = newLinkedList(SPLITTER.split(sharedPreferences.getString(PREF_RECENT_REPOS, "")));
     }
 
     /**
-     * Add repository to recent list
+     * Load the recent-repo list, add the supplied repository to it's head and store the list.
+     * <p/>
+     * It's expected that this would be called just once for a Repo-viewing activity.
      *
      * @param repo
-     * @return this helper
      */
-    public RecentReposHelper add(final IRepositoryIdProvider repo) {
-        return repo != null ? add(repo.generateId()) : this;
-    }
+    public void storeRepoVisit(IRepositoryIdProvider repo) {
+        if (repo == null)
+            return;
 
-    /**
-     * Add id to recent list
-     *
-     * @param repoId
-     * @return this helper
-     */
-    public RecentReposHelper add(final String repoId) {
-        if (!TextUtils.isEmpty(repoId)) {
-            recentRepos.remove(repoId);
-            recentRepos.addFirst(repoId);
-            trimRecentRepos();
-        }
-        return this;
-    }
+        String repoId = repo.generateId();
 
-    private void trimRecentRepos() {
+        if (TextUtils.isEmpty(repoId))
+            return;
+
+        load();
+        recentRepos.remove(repoId);
+        recentRepos.addFirst(repoId);
+
         while (recentRepos.size() > MAX_RECENT_REPOS)
             recentRepos.removeLast();
-    }
 
-    private File getRecentReposFile() {
-        return context.getFileStreamPath(FILE_RECENT_REPOS);
-    }
-
-    /**
-     * Persist recent list
-     *
-     * @return this helper
-     */
-    public RecentReposHelper save() {
-        new RequestWriter(getRecentReposFile(), VERSION_RECENT_REPOS).write(recentRepos);
-        return this;
-    }
-
-    /**
-     * Is the given repository id contained in the recent list?
-     *
-     * @param repoId
-     * @return true if recent, false otherwise
-     */
-    public boolean contains(String repoId) {
-        return !TextUtils.isEmpty(repoId) && recentRepos.contains(repoId);
+        savePrefsFrom(sharedPreferences.edit().putString(PREF_RECENT_REPOS, JOINER.join(recentRepos)));
     }
 
     /**

app/src/main/java/com/github/mobile/android/repo/RepoListFragment.java
@@ -44,6 +44,7 @@ public class RepoListFragment extends ListLoadingFragment<Repository> implements
 
     private final AtomicReference<User> org = new AtomicReference<User>();
 
+    @Inject
     private RecentReposHelper recentReposHelper;
 
     private final AtomicReference<RecentRepos> recentReposRef = new AtomicReference<RecentRepos>();
@@ -51,7 +52,6 @@ public class RepoListFragment extends ListLoadingFragment<Repository> implements
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        recentReposHelper = new RecentReposHelper(activity);
         org.set(((HomeActivity) activity).registerOrgSelectionListener(this));
     }
 
@@ -90,16 +90,15 @@ public class RepoListFragment extends ListLoadingFragment<Repository> implements
     @Override
     public void onListItemClick(ListView list, View v, int position, long id) {
         Repository repo = (Repository) list.getItemAtPosition(position);
-        recentReposHelper.add(repo);
         startActivity(RepositoryViewActivity.createIntent(repo));
-        refresh();
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
+    public void onResume() {
+        super.onResume();
+        recentReposHelper.load();
 
-        recentReposHelper.save();
+        refresh();
     }
 
     @Override

app/src/main/java/com/github/mobile/android/ui/repo/RepositoryViewActivity.java
@@ -12,6 +12,7 @@ import com.actionbarsherlock.view.MenuItem;
 import com.github.mobile.android.HomeActivity;
 import com.github.mobile.android.R.id;
 import com.github.mobile.android.R.layout;
+import com.github.mobile.android.repo.RecentReposHelper;
 import com.github.mobile.android.util.AvatarHelper;
 import com.github.mobile.android.util.GitHubIntents.Builder;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockFragmentActivity;
@@ -50,10 +51,14 @@ public class RepositoryViewActivity extends RoboSherlockFragmentActivity {
     @Inject
     private AvatarHelper avatarHelper;
 
+    @Inject
+    private RecentReposHelper recentReposHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        recentReposHelper.storeRepoVisit(repository);
         setContentView(layout.pager_with_title);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(repository.getName());

commit 7ac319bf2acacc444aebc79c0d233d78bfa9eecd

add integration test for Org switcher and Org repo-list display
This just pages through the Orgs and checks that the repo list updates
to show at least one repo name known to be in the selected org. I've
added this test because I recently unwittingly broke the Org switcher
with 6708c75- the associated code is fragile and I'd like to make sure I
don't break it again! I'm a little concerned that the integration test
itself might be rather brittle but time will tell on that front...

This commit includes a tweak to cope with pre-ICS Spinner by ABS:
Instead of android.widget.Spinner, ABS uses
'com.actionbarsherlock.internal.widget.IcsSpinner' for the actionbar
dropdown on pre-ICS devices, so the integration test was failing to find
the dropdown on pre-ics devices.

app/src/main/java/com/github/mobile/android/guice/GitHubAccountScope.java
@@ -1,12 +1,10 @@
 package com.github.mobile.android.guice;
 
-import static android.accounts.AccountManager.KEY_ACCOUNT_NAME;
-import static com.github.mobile.android.authenticator.Constants.GITHUB_ACCOUNT_TYPE;
+import static com.github.mobile.android.util.AccountHelper.demandCurrentAccount;
 import static com.google.common.base.Preconditions.checkState;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
-import android.os.Bundle;
 import android.util.Log;
 
 import com.github.mobile.android.authenticator.GitHubAccount;
@@ -46,13 +44,13 @@ public class GitHubAccountScope extends ScopeBase {
     private final ThreadLocal<GitHubAccount> currentAccount = new ThreadLocal<GitHubAccount>();
 
     private final Map<GitHubAccount, Map<Key<?>, Object>> repoScopeMaps = new MapMaker().
-            makeComputingMap(new Function<GitHubAccount, Map<Key<?>, Object>>() {
-                public Map<Key<?>, Object> apply(GitHubAccount account) {
-                    ConcurrentMap<Key<?>, Object> accountScopeMap = new MapMaker().makeMap();
-                    accountScopeMap.put(GITHUB_ACCOUNT_KEY, account);
-                    return accountScopeMap;
-                }
-            });
+        makeComputingMap(new Function<GitHubAccount, Map<Key<?>, Object>>() {
+            public Map<Key<?>, Object> apply(GitHubAccount account) {
+                ConcurrentMap<Key<?>, Object> accountScopeMap = new MapMaker().makeMap();
+                accountScopeMap.put(GITHUB_ACCOUNT_KEY, account);
+                return accountScopeMap;
+            }
+        });
 
     /**
      * Enters scope once we've ensured the user has a valid account.
@@ -91,28 +89,5 @@ public class GitHubAccountScope extends ScopeBase {
         return repoScopeMaps.get(account);
     }
 
-    private Account demandCurrentAccount(AccountManager accountManager, Activity activityUsedToStartLoginProcess) {
-        Account[] accounts;
-        Log.d(TAG, "Getting current account...");
-        try {
-            while ((accounts = accountManager.
-                    getAccountsByTypeAndFeatures(GITHUB_ACCOUNT_TYPE, null, null, null).getResult()).length == 0) {
-                Log.d(TAG, "Currently zero GitHub accounts... activity=" + activityUsedToStartLoginProcess);
-                if (activityUsedToStartLoginProcess == null)
-                    throw new RuntimeException("Can't create new GitHub account - no activity available");
 
-                Bundle result = accountManager.addAccount(GITHUB_ACCOUNT_TYPE, null, null, null,
-                        activityUsedToStartLoginProcess, null, null).getResult();
-
-                Log.i(TAG, "Added account " + result.getString(KEY_ACCOUNT_NAME));
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "Problem getting a Github account...", e);
-            throw new RuntimeException(e);
-        }
-
-        Account account = accounts[0];
-        Log.d(TAG, "Returning account " + account.name);
-        return account;
-    }
 }

app/src/main/java/com/github/mobile/android/repo/RecentReposHelper.java
@@ -44,7 +44,7 @@ public class RecentReposHelper {
 
     private static final int VERSION_RECENT_REPOS = 2;
 
-    private static final Function<Repository, String> REPO_NAME = new Function<Repository, String>() {
+    public static final Function<Repository, String> REPO_NAME = new Function<Repository, String>() {
         public String apply(Repository repo) {
             return repo.getName();
         }

app/src/main/java/com/github/mobile/android/util/AccountHelper.java
@@ -1,14 +1,20 @@
 package com.github.mobile.android.util;
 
+import static android.accounts.AccountManager.KEY_ACCOUNT_NAME;
 import static com.github.mobile.android.authenticator.Constants.GITHUB_ACCOUNT_TYPE;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Helpers for accessing {@link AccountManager}
  */
 public class AccountHelper {
+
+    private static final String TAG = "GH.AccountHelper";
 
     /**
      * Get login name of configured account
@@ -19,5 +25,31 @@ public class AccountHelper {
     public static String getLogin(final Context context) {
         final Account[] accounts = AccountManager.get(context).getAccountsByType(GITHUB_ACCOUNT_TYPE);
         return accounts.length > 0 ? accounts[0].name : null;
+    }
+
+    public static Account demandCurrentAccount(AccountManager accountManager,
+        Activity activityUsedToStartLoginProcess) {
+        Account[] accounts;
+        Log.d(TAG, "Getting current account...");
+        try {
+            while ((accounts = accountManager.
+                getAccountsByTypeAndFeatures(GITHUB_ACCOUNT_TYPE, null, null, null).getResult()).length == 0) {
+                Log.d(TAG, "Currently zero GitHub accounts... activity=" + activityUsedToStartLoginProcess);
+                if (activityUsedToStartLoginProcess == null)
+                    throw new RuntimeException("Can't create new GitHub account - no activity available");
+
+                Bundle result = accountManager.addAccount(GITHUB_ACCOUNT_TYPE, null, null, null,
+                    activityUsedToStartLoginProcess, null, null).getResult();
+
+                Log.i(TAG, "Added account " + result.getString(KEY_ACCOUNT_NAME));
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Problem getting a Github account...", e);
+            throw new RuntimeException(e);
+        }
+
+        Account account = accounts[0];
+        Log.d(TAG, "Returning account " + account.name);
+        return account;
     }
 }

integration-tests/pom.xml
@@ -50,6 +50,11 @@
             <scope>provided</scope>
             <type>jar</type>
         </dependency>
+        <dependency>
+            <groupId>com.github.rtyley</groupId>
+            <artifactId>android-screenshot-celebrity</artifactId>
+            <version>1.8</version>
+        </dependency>
     </dependencies>
     <build>
         <resources>

integration-tests/src/main/java/com/github/mobile/android/HomeActivityTest.java
@@ -1,15 +1,141 @@
 package com.github.mobile.android;
 
+import static com.github.mobile.android.R.id.tv_org_name;
+import static com.github.mobile.android.R.id.tv_repo_name;
+import static com.github.mobile.android.repo.RecentReposHelper.REPO_NAME;
+import static com.github.mobile.android.test.TestUserAccountUtil.ensureValidGitHubAccountAvailable;
+import static com.github.mobile.android.util.AccountHelper.demandCurrentAccount;
+import static com.github.rtyley.android.screenshot.celebrity.Screenshots.poseForScreenshot;
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.support.v4.view.ViewPager;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.mobile.android.ui.user.UserPagerAdapter;
+import com.google.common.base.Joiner;
+import com.jayway.android.robotium.solo.Solo;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.OrganizationService;
+import org.eclipse.egit.github.core.service.RepositoryService;
 
 public class HomeActivityTest extends ActivityInstrumentationTestCase2<HomeActivity> {
+
+    private static final String TAG = "HomeActivityTest";
+
+    private Solo solo;
+    private Account account;
+    private OrganizationService organizationService;
+    private RepositoryService repositoryService;
 
     public HomeActivityTest() {
         super(HomeActivity.class);
     }
 
-    public void testHomeActivityCanActuallyStart() {
-        getActivity(); // a smoke-test for the Guice configuration
+    @Override
+    public void setUp() throws Exception {
+        ensureValidGitHubAccountAvailable(getInstrumentation());
+        solo = new Solo(getInstrumentation());
+
+        AccountManager accountManager = AccountManager.get(getInstrumentation().getTargetContext());
+        account = demandCurrentAccount(accountManager, null);
+        GitHubClient gitHubClient = new GitHubClient();
+        gitHubClient.setCredentials(account.name, accountManager.getPassword(account));
+
+        organizationService = new OrganizationService(gitHubClient);
+        repositoryService = new RepositoryService(gitHubClient);
     }
 
+    @Override
+    protected void tearDown() throws Exception {
+        if (!solo.getAllOpenedActivities().isEmpty()) {
+            Log.d(TAG, "tearDown will finishOpenedActivities()");
+            solo.finishOpenedActivities(); // this method hangs if no activities have been started in the test
+        }
+    }
+
+    @MediumTest
+    public void testReposListedCorrectlyWhenUsingTheOrgSwitcher() throws Exception {
+        HomeActivity activity = getActivity();
+
+        List<User> orgs = organizationService.getOrganizations();
+
+        poseForScreenshot();
+        solo.sleep(6000);
+        poseForScreenshot();
+        solo.sleep(1000);
+        poseForScreenshot();
+
+        ViewPager viewPager = (ViewPager) solo.getView(ViewPager.class, 0);
+        setViewPagerToIndex(viewPager, titlesOn(viewPager).indexOf("Repos"));
+
+        solo.sleep(5000);
+        poseForScreenshot();
+
+        Log.d(TAG, "Got " + orgs.size() + " orgs for account " + account.name);
+        for (int i = 0; i < orgs.size(); ++i) {
+            setSelectedNavigationItem(activity, i + 1);
+            solo.sleep(1000);
+            poseForScreenshot();
+
+            String orgName = textIn(activity.findViewById(tv_org_name));
+            String repoIdsInOrg = loadRepoIdsFor(orgName, repositoryService);
+
+            solo.sleep(1000);
+            poseForScreenshot();
+
+            String displayedRepoId = textIn(viewPager.findViewById(tv_repo_name)); // first repo shown in list
+            Log.d(TAG, "org text = " + orgName + " repo name = " + displayedRepoId);
+            poseForScreenshot();
+            assertThat(repoIdsInOrg, containsString(displayedRepoId));
+        }
+    }
+
+    private String loadRepoIdsFor(String orgName, RepositoryService repositoryService) throws IOException {
+        List<Repository> orgRepositories = repositoryService.getOrgRepositories(orgName);
+        return Joiner.on(",").join(transform(orgRepositories, REPO_NAME));
+    }
+
+    private String textIn(View textView) {
+        return ((TextView) textView).getText().toString();
+    }
+
+    private List<String> titlesOn(ViewPager viewPager) {
+        List<String> titles = newArrayList();
+        UserPagerAdapter titleProvider = (UserPagerAdapter) viewPager.getAdapter();
+        for (int i = 0; i < viewPager.getChildCount(); ++i) {
+            titles.add(titleProvider.getTitle(i));
+        }
+        Log.d(TAG, "Found titles " + titles);
+        return titles;
+    }
+
+    private void setViewPagerToIndex(final ViewPager viewPager, final int index) {
+        getInstrumentation().runOnMainSync(new Runnable() {
+            public void run() {
+                viewPager.setCurrentItem(index);
+            }
+        });
+    }
+
+    private void setSelectedNavigationItem(final HomeActivity activity, final int index) {
+        getInstrumentation().runOnMainSync(new Runnable() {
+            public void run() {
+                activity.getSupportActionBar().setSelectedNavigationItem(index);
+            }
+        });
+    }
 }