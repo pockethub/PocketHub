1bd67e70eddb68467d8db9a33792936d7a99379c
Kevin Sawicki <kevin@github.com>
2012-04-24 10:46:48 -0700

    Update event_item layout to include icon
      * Place avatar on left side of event_item
      * Add octocons icon to the left of time text
      * Make repository and other important text bold

--- app/src/main/java/com/github/mobile/android/ui/user/NewsEventViewHolder.java
+++ app/src/main/java/com/github/mobile/android/ui/user/NewsEventViewHolder.java
@@ -27,6 +27,7 @@ import android.widget.TextView;
 import com.github.mobile.android.R.id;
 import com.github.mobile.android.util.AvatarHelper;
 import com.github.mobile.android.util.Time;
+import com.github.mobile.android.util.TypefaceHelper;
 import com.madgag.android.listviews.ViewHolder;
 
 import java.text.MessageFormat;
@@ -95,6 +96,8 @@ public class NewsEventViewHolder implements ViewHolder<Event> {
 
     private final TextView eventText;
 
+    private final TextView iconText;
+
     private final TextView dateText;
 
     /**
@@ -107,6 +110,8 @@ public class NewsEventViewHolder implements ViewHolder<Event> {
         this.avatarHelper = avatarHelper;
         avatarView = (ImageView) view.findViewById(id.iv_gravatar);
         eventText = (TextView) view.findViewById(id.tv_event);
+        iconText = (TextView) view.findViewById(id.tv_event_icon);
+        TypefaceHelper.setOctocons(iconText);
         dateText = (TextView) view.findViewById(id.tv_event_date);
     }
 
@@ -115,13 +120,16 @@ public class NewsEventViewHolder implements ViewHolder<Event> {
 
         String relativeTime = Time.relativeTimeFor(event.getCreatedAt()).toString();
         String actor = "<b>" + event.getActor().getLogin() + "</b>";
-        String repoName = event.getRepo().getName();
+        String repoName = "<b>" + event.getRepo().getName() + "</b>";
         String type = event.getType();
         String text = null;
+        String icon = null;
 
-        if (TYPE_COMMIT_COMMENT.equals(type))
+        if (TYPE_COMMIT_COMMENT.equals(type)) {
+            icon = "\uf243";
             text = MessageFormat.format("{0} commented on commit on {1}", actor, repoName);
-        else if (TYPE_CREATE.equals(type)) {
+        } else if (TYPE_CREATE.equals(type)) {
+            icon = "\uf203";
             CreatePayload payload = (CreatePayload) event.getPayload();
             String refType = payload.getRefType();
             String created;
@@ -132,17 +140,22 @@ public class NewsEventViewHolder implements ViewHolder<Event> {
 
             text = MessageFormat.format("{0} created {1} {2}", actor, refType, created);
         } else if (TYPE_DELETE.equals(type)) {
+            icon = "\uf204";
             DeletePayload payload = (DeletePayload) event.getPayload();
             String refType = payload.getRefType();
             text = MessageFormat.format("{0} deleted {1} {2} at {3}", actor, refType, payload.getRef(), repoName);
-        } else if (TYPE_DOWNLOAD.equals(type))
+        } else if (TYPE_DOWNLOAD.equals(type)) {
+            icon = "\uf212";
             text = MessageFormat.format("{0} uploaded a file to {1}", actor, repoName);
-        else if (TYPE_FOLLOW.equals(type))
+        } else if (TYPE_FOLLOW.equals(type)) {
+            icon = "\uf228";
             text = MessageFormat.format("{0} started following {1}", actor, ((FollowPayload) event.getPayload())
                     .getTarget().getLogin());
-        else if (TYPE_FORK.equals(type))
+        } else if (TYPE_FORK.equals(type)) {
+            icon = "\uf202";
             text = MessageFormat.format("{0} forked repository {1}", actor, repoName);
-        else if (TYPE_GIST.equals(type)) {
+        } else if (TYPE_GIST.equals(type)) {
+            icon = "\uf214";
             GistPayload payload = (GistPayload) event.getPayload();
             String action;
             if ("create".equals(payload.getAction()))
@@ -152,36 +165,50 @@ public class NewsEventViewHolder implements ViewHolder<Event> {
             else
                 action = payload.getAction();
             text = MessageFormat.format("{0} {1} Gist {2}", actor, action, payload.getGist().getId());
-        } else if (TYPE_GOLLUM.equals(type))
+        } else if (TYPE_GOLLUM.equals(type)) {
+            icon = "\uf207";
             text = MessageFormat.format("{0} updated the wiki in {1}", actor, repoName);
-        else if (TYPE_ISSUE_COMMENT.equals(type)) {
+        } else if (TYPE_ISSUE_COMMENT.equals(type)) {
+            icon = "\uf241";
             Issue issue = ((IssueCommentPayload) event.getPayload()).getIssue();
-            text = MessageFormat.format("{0} commented on issue {1} on {2}", actor,
+            text = MessageFormat.format("{0} commented on <b>issue {1}</b> on {2}", actor,
                     Integer.toString(issue.getNumber()), repoName);
         } else if (TYPE_ISSUES.equals(type)) {
             IssuesPayload payload = (IssuesPayload) event.getPayload();
-            text = MessageFormat.format("{0} {1} issue {2} on {3}", actor, payload.getAction(),
+            String action = payload.getAction();
+            if ("opened".equals(action))
+                icon = "\uf238";
+            else if ("reopened".equals(action))
+                icon = "\uf239";
+            else if ("closed".equals(action))
+                icon = "\uf240";
+            text = MessageFormat.format("{0} {1} <b>issue {2}</b> on {3}", actor, action,
                     Integer.toString(payload.getIssue().getNumber()), repoName);
-        } else if (TYPE_MEMBER.equals(type))
+        } else if (TYPE_MEMBER.equals(type)) {
+            icon = "\uf226";
             text = MessageFormat.format("{0} was added as a collaborator to {1}", actor, repoName);
-        else if (TYPE_PUBLIC.equals(type))
+        } else if (TYPE_PUBLIC.equals(type))
             text = MessageFormat.format("{0} open sourced repository {1}", actor, repoName);
         else if (TYPE_PULL_REQUEST.equals(type)) {
+            icon = "\uf234";
             PullRequestPayload payload = (PullRequestPayload) event.getPayload();
             String action = payload.getAction();
             if ("synchronize".equals(action))
                 action = "updated";
-            text = MessageFormat.format("{0} {1} pull request {2} on {3}", actor, action,
+            text = MessageFormat.format("{0} {1} <b>pull request {2}</b> on {3}", actor, action,
                     Integer.toString(payload.getPullRequest().getNumber()), repoName);
-        } else if (TYPE_PULL_REQUEST_REVIEW_COMMENT.equals(type))
+        } else if (TYPE_PULL_REQUEST_REVIEW_COMMENT.equals(type)) {
+            icon = "\uf243";
             text = MessageFormat.format("{0} commented on {1}", actor, repoName);
-        else if (TYPE_PUSH.equals(type)) {
+        } else if (TYPE_PUSH.equals(type)) {
+            icon = "\uf205";
             PushPayload payload = (PushPayload) event.getPayload();
             String ref = payload.getRef();
             if (ref.startsWith("refs/heads/"))
                 ref = ref.substring(11);
-            text = MessageFormat.format("{0} pushed to {1} at {2}", actor, ref, repoName);
+            text = MessageFormat.format("{0} pushed to <b>{1}</b> at {2}", actor, ref, repoName);
         } else if (TYPE_TEAM_ADD.equals(type)) {
+            icon = "\uf226";
             TeamAddPayload payload = (TeamAddPayload) event.getPayload();
             Team team = payload.getTeam();
             String teamName;
@@ -196,9 +223,12 @@ public class NewsEventViewHolder implements ViewHolder<Event> {
             else
                 value = payload.getRepo().getName();
             text = MessageFormat.format("{0} added {1} to team{2}", actor, value, teamName);
-        } else if (TYPE_WATCH.equals(type))
+        } else if (TYPE_WATCH.equals(type)) {
+            icon = "\uf229";
             text = MessageFormat.format("{0} started watching {1}", actor, repoName);
+        }
 
+        iconText.setText(icon);
         eventText.setText(Html.fromHtml(text));
         dateText.setText(relativeTime);
     }
