d19572ff7eb34be74365d812088ed63c8ad31059
Kevin Sawicki <kevin@github.com>
2012-04-29 12:29:55 -0700

    Add Apache license header to .java & .xml files

--- app/src/main/java/com/github/mobile/util/ToastUtils.java
+++ app/src/main/java/com/github/mobile/util/ToastUtils.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.github.mobile.util;
 
 import static android.widget.Toast.LENGTH_LONG;
