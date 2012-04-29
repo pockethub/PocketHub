2ffb85cd818425a552349813b8a89ce2c469fc94
Kevin Sawicki <kevin@github.com>
2012-04-29 14:42:53 -0700

    Move issue comparator into core package

--- app/src/main/java/com/github/mobile/core/issue/CreatedAtComparator.java
+++ app/src/main/java/com/github/mobile/core/issue/CreatedAtComparator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.mobile.issue;
+package com.github.mobile.core.issue;
 
 import java.util.Comparator;
 
