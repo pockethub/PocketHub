14b50aa89ec82a58df13bf74b8927bf5917f915e
Kevin Sawicki <kevin@github.com>
2012-04-29 18:51:30 -0700

    Add unit tests of UserComparator

--- app/src/main/java/com/github/mobile/core/user/UserComparator.java
+++ app/src/main/java/com/github/mobile/core/user/UserComparator.java
@@ -42,12 +42,13 @@ public class UserComparator implements Comparator<User> {
     @Override
     public int compare(final User lhs, final User rhs) {
         final String lhsLogin = lhs.getLogin();
-        if (lhsLogin.equals(login))
-            return -1;
-
         final String rhsLogin = rhs.getLogin();
+
+        if (lhsLogin.equals(login))
+            return rhsLogin.equals(login) ? 0 : -1;
+
         if (rhsLogin.equals(login))
-            return 1;
+            return lhsLogin.equals(login) ? 0 : 1;
 
         return lhsLogin.compareToIgnoreCase(rhsLogin);
     }
---
+++ app/src/test/java/com/github/mobile/core/user/UserComparatorTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012 GitHub Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.mobile.core.user;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.github.mobile.core.GitHubAccount;
+
+import org.eclipse.egit.github.core.User;
+import org.junit.Test;
+
+/**
+ * Unit tests of {@link UserComparator}
+ */
+public class UserComparatorTest {
+
+    /**
+     * Test sorting of users that match login
+     */
+    @Test
+    public void loginMatch() {
+        GitHubAccount account = new GitHubAccount("m", "n");
+        UserComparator comparator = new UserComparator(account);
+
+        assertTrue(comparator.compare(new User().setLogin("m"), new User().setLogin("a")) < 0);
+        assertTrue(comparator.compare(new User().setLogin("a"), new User().setLogin("m")) > 0);
+        assertTrue(comparator.compare(new User().setLogin("m"), new User().setLogin("z")) < 0);
+        assertTrue(comparator.compare(new User().setLogin("z"), new User().setLogin("m")) > 0);
+        assertEquals(0, comparator.compare(new User().setLogin("m"), new User().setLogin("m")));
+    }
+
+    /**
+     * Test sorting of users that don't match login
+     */
+    @Test
+    public void noLoginMatch() {
+        GitHubAccount account = new GitHubAccount("m", "n");
+        UserComparator comparator = new UserComparator(account);
+
+        assertTrue(comparator.compare(new User().setLogin("a"), new User().setLogin("c")) < 0);
+        assertTrue(comparator.compare(new User().setLogin("db"), new User().setLogin("da")) > 0);
+    }
+}
