9fee6c03ae673d605ff70a7fd6c455e1adb09252
Kevin Sawicki <kevin@github.com>
2012-05-17 11:14:09 -0700

    Show correct error message when collaborators fail to load

--- app/src/main/java/com/github/mobile/ui/issue/AssigneeDialog.java
+++ app/src/main/java/com/github/mobile/ui/issue/AssigneeDialog.java
@@ -86,7 +86,7 @@ public class AssigneeDialog {
             protected void onException(Exception e) throws RuntimeException {
                 super.onException(e);
 
-                ToastUtils.show(activity, e, string.error_assignee_update);
+                ToastUtils.show(activity, e, string.error_collaborators_load);
             }
 
             @Override
