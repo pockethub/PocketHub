6f843884ba1b2cc6276de5a54a2e1246042e959e
Kevin Sawicki <kevin@github.com>
2012-07-19 17:27:22 -0700

    Return to repository view from CommitViewActivity

--- app/src/main/java/com/github/mobile/ui/commit/CommitViewActivity.java
+++ app/src/main/java/com/github/mobile/ui/commit/CommitViewActivity.java
@@ -15,14 +15,18 @@
  */
 package com.github.mobile.ui.commit;
 
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
 import static com.github.mobile.Intents.EXTRA_BASE;
 import static com.github.mobile.Intents.EXTRA_REPOSITORY;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.MenuItem;
 import com.github.mobile.Intents.Builder;
 import com.github.mobile.ui.DialogFragmentActivity;
+import com.github.mobile.ui.repo.RepositoryViewActivity;
 import com.github.mobile.util.AvatarLoader;
 import com.google.inject.Inject;
 import com.viewpagerindicator.R.layout;
@@ -51,6 +55,9 @@ public class CommitViewActivity extends DialogFragmentActivity {
         return builder.toIntent();
     }
 
+    @InjectExtra(EXTRA_REPOSITORY)
+    private Repository repository;
+
     @InjectExtra(EXTRA_BASE)
     private String id;
 
@@ -63,12 +70,23 @@ public class CommitViewActivity extends DialogFragmentActivity {
 
         setContentView(layout.commit);
 
-        Intent intent = getIntent();
-        Repository repository = (Repository) intent
-                .getSerializableExtra(EXTRA_REPOSITORY);
         ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(id);
         actionBar.setSubtitle(repository.generateId());
         avatars.bind(actionBar, repository.getOwner());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            Intent intent = RepositoryViewActivity.createIntent(repository);
+            intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
