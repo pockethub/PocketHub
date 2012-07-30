f253f2f2c5e2a7f953abddb607ecc6efb4540a0a
Kevin Sawicki <kevin@github.com>
2012-07-30 08:25:27 -0700

    Open repository when search query is an exact match
    If the search query entered is an exact owner/name repository
    match then open the repository directly and finish the search
    activity.
    
    Closes issue #80

--- app/src/main/java/com/github/mobile/ui/repo/SearchRepositoryListFragment.java
+++ app/src/main/java/com/github/mobile/ui/repo/SearchRepositoryListFragment.java
@@ -15,8 +15,10 @@
  */
 package com.github.mobile.ui.repo;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.content.Loader;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ListView;
 
@@ -28,10 +30,13 @@ import com.github.mobile.ui.ItemListFragment;
 import com.github.mobile.ui.ItemView;
 import com.google.inject.Inject;
 
+import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.SearchRepository;
 import org.eclipse.egit.github.core.service.RepositoryService;
 
@@ -86,13 +91,45 @@ public class SearchRepositoryListFragment extends
         }.execute();
     }
 
+    /**
+     * Check if the search query is an exact repository name/owner match and
+     * open the repository activity and finish the current activity when it is
+     *
+     * @param query
+     * @return true if query opened as repository, false otherwise
+     */
+    private boolean openRepositoryMatch(final String query) {
+        if (TextUtils.isEmpty(query))
+            return false;
+
+        RepositoryId repoId = RepositoryId.createFromId(query.trim());
+        if (repoId == null)
+            return false;
+
+        Repository repo;
+        try {
+            repo = service.getRepository(repoId);
+        } catch (IOException e) {
+            return false;
+        }
+
+        startActivity(RepositoryViewActivity.createIntent(repo));
+        final Activity activity = getActivity();
+        if (activity != null)
+            activity.finish();
+        return true;
+    }
+
     @Override
     public Loader<List<SearchRepository>> onCreateLoader(int id, Bundle args) {
         return new ThrowableLoader<List<SearchRepository>>(getActivity(), items) {
 
             @Override
             public List<SearchRepository> loadData() throws Exception {
-                return service.searchRepositories(query);
+                if (openRepositoryMatch(query))
+                    return Collections.emptyList();
+                else
+                    return service.searchRepositories(query);
             }
         };
     }
