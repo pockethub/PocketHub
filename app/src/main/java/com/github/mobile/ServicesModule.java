72eed3ecc1fad72e76514cf038d8524da99f9fcf
Trevor Basinger <trevor.basinger@gmail.com>
2012-09-18 14:20:21 -0500

    Adding OAuthService to ServiceModule

--- app/src/main/java/com/github/mobile/ServicesModule.java
+++ app/src/main/java/com/github/mobile/ServicesModule.java
@@ -31,6 +31,7 @@ import org.eclipse.egit.github.core.service.IssueService;
 import org.eclipse.egit.github.core.service.LabelService;
 import org.eclipse.egit.github.core.service.MilestoneService;
 import org.eclipse.egit.github.core.service.OrganizationService;
+import org.eclipse.egit.github.core.service.OAuthService;
 import org.eclipse.egit.github.core.service.PullRequestService;
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.eclipse.egit.github.core.service.UserService;
@@ -113,5 +114,10 @@ public class ServicesModule extends AbstractModule {
     @Provides
     DataService dataService(GitHubClient client) {
         return new DataService(client);
+    }
+
+    @Provides
+    OAuthService oAuthService(GitHubClient client) {
+      return new OAuthService(client);
     }
 }
