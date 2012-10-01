21aaf5c6c1a0485d5e2ad64fc2a55173ead9ec39
Artur Termenji <atermenji@gmail.com>
2012-10-02 00:09:24 +0300

    Add following/unfollowing users

---
+++ app/src/main/java/com/github/mobile/core/user/FollowUserTask.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2012 GitHub Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.mobile.core.user;
+
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.service.UserService;
+
+import android.accounts.Account;
+import android.content.Context;
+import android.util.Log;
+
+import com.github.mobile.accounts.AuthenticatedUserTask;
+import com.google.inject.Inject;
+
+/**
+ * Task to follow a user
+ */
+public class FollowUserTask extends AuthenticatedUserTask<User> {
+
+    private static final String TAG = "FollowUserTask";
+
+    @Inject
+    private UserService service;
+
+    private final String login;
+
+    /**
+     * Create task for context and login
+     *
+     * @param context
+     * @param login
+     */
+    public FollowUserTask(Context context, String login) {
+        super(context);
+
+        this.login = login;
+    }
+
+    @Override
+    protected User run(Account account) throws Exception {
+        if (service.isFollowing(login)) {
+            service.unfollow(login);
+        } else {
+            service.follow(login);
+        }
+
+        return service.getUser(login);
+    }
+
+    @Override
+    protected void onException(Exception e) throws RuntimeException {
+        super.onException(e);
+
+        Log.d(TAG, "Exception following/unfollowing user", e);
+    }
+}
