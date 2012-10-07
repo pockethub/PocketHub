c40456bc1f50fa899a9ea09d0713355693051bbd
Artur Termenji <atermenji@gmail.com>
2012-10-08 00:36:14 +0300

    Added a possibility to star/unstar repositories. refs issue #236

--- app/src/main/java/com/github/mobile/ui/repo/RepositoryViewActivity.java
+++ app/src/main/java/com/github/mobile/ui/repo/RepositoryViewActivity.java
@@ -23,30 +23,35 @@ import static com.github.mobile.util.TypefaceUtils.ICON_CODE;
 import static com.github.mobile.util.TypefaceUtils.ICON_COMMIT;
 import static com.github.mobile.util.TypefaceUtils.ICON_ISSUE_OPEN;
 import static com.github.mobile.util.TypefaceUtils.ICON_NEWS;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.ProgressBar;
-
-import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.view.MenuItem;
-import com.github.kevinsawicki.wishlist.ViewUtils;
-import com.github.mobile.Intents.Builder;
-import com.github.mobile.R.id;
-import com.github.mobile.R.layout;
-import com.github.mobile.R.string;
-import com.github.mobile.core.repo.RefreshRepositoryTask;
-import com.github.mobile.core.repo.RepositoryUtils;
-import com.github.mobile.ui.TabPagerActivity;
-import com.github.mobile.ui.user.HomeActivity;
-import com.github.mobile.util.AvatarLoader;
-import com.github.mobile.util.ToastUtils;
-import com.google.inject.Inject;
 
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.User;
 
 import roboguice.inject.InjectExtra;
 import roboguice.inject.InjectView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.github.kevinsawicki.wishlist.ViewUtils;
+import com.github.mobile.Intents.Builder;
+import com.github.mobile.R.id;
+import com.github.mobile.R.layout;
+import com.github.mobile.R.menu;
+import com.github.mobile.R.string;
+import com.github.mobile.core.repo.RefreshRepositoryTask;
+import com.github.mobile.core.repo.RepositoryUtils;
+import com.github.mobile.core.repo.UnstarRepositoryTask;
+import com.github.mobile.core.repo.StarRepositoryTask;
+import com.github.mobile.core.repo.StarringRepositoryTask;
+import com.github.mobile.ui.TabPagerActivity;
+import com.github.mobile.ui.user.HomeActivity;
+import com.github.mobile.util.AvatarLoader;
+import com.github.mobile.util.ToastUtils;
+import com.google.inject.Inject;
 
 /**
  * Activity to view a repository
@@ -72,6 +77,10 @@ public class RepositoryViewActivity extends
 
     @InjectView(id.pb_loading)
     private ProgressBar loadingBar;
+
+    private boolean isStarring;
+
+    private boolean starringStatusChecked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,6 +124,23 @@ public class RepositoryViewActivity extends
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu optionsMenu) {
+        getSupportMenuInflater().inflate(menu.repository_star, optionsMenu);
+
+        return super.onCreateOptionsMenu(optionsMenu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem followItem = menu.findItem(id.m_star);
+
+        followItem.setVisible(starringStatusChecked);
+        followItem.setTitle(isStarring ? string.unstar : string.star);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onSearchRequested() {
         if (pager.getCurrentItem() == 1) {
             Bundle args = new Bundle();
@@ -137,11 +163,15 @@ public class RepositoryViewActivity extends
         configureTabPager();
         ViewUtils.setGone(loadingBar, true);
         setGone(false);
+        checkStarringRepositoryStatus();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+        case id.m_star:
+            starRepository();
+            return true;
         case android.R.id.home:
             Intent intent = new Intent(this, HomeActivity.class);
             intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_SINGLE_TOP);
@@ -182,5 +212,59 @@ public class RepositoryViewActivity extends
         default:
             return super.getIcon(position);
         }
+    }
+
+    private void starRepository() {
+        if (isStarring)
+            new UnstarRepositoryTask(this, repository) {
+
+                @Override
+                protected void onSuccess(Void v) throws Exception {
+                    super.onSuccess(v);
+
+                    isStarring = !isStarring;
+                }
+
+                @Override
+                protected void onException(Exception e) throws RuntimeException {
+                    super.onException(e);
+
+                    ToastUtils.show(RepositoryViewActivity.this,
+                            string.error_unstarring_repository);
+                }
+            }.start();
+        else
+            new StarRepositoryTask(this, repository) {
+
+                @Override
+                protected void onSuccess(Void v) throws Exception {
+                    super.onSuccess(v);
+
+                    isStarring = !isStarring;
+                }
+
+                @Override
+                protected void onException(Exception e) throws RuntimeException {
+                    super.onException(e);
+
+                    ToastUtils.show(RepositoryViewActivity.this,
+                            string.error_starring_repository);
+                }
+            }.start();
+    }
+
+    private void checkStarringRepositoryStatus() {
+        starringStatusChecked = false;
+        new StarringRepositoryTask(this, repository) {
+
+            @Override
+            protected void onSuccess(Boolean watching) throws Exception {
+                super.onSuccess(watching);
+
+                isStarring = watching;
+                starringStatusChecked = true;
+                invalidateOptionsMenu();
+            }
+        }.execute();
     }
 }
