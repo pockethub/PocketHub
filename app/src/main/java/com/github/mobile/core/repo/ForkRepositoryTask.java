81c2b4d4262527aa233488f6f1e8e508039501bc
Artur Termenji <atermenji@gmail.com>
2012-10-15 23:48:41 +0300

    Add possibility to fork a repository, refs #106

---
+++ app/src/main/java/com/github/mobile/core/repo/ForkRepositoryTask.java
@@ -0,0 +1,63 @@
+package com.github.mobile.core.repo;
+
+import org.eclipse.egit.github.core.IRepositoryIdProvider;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import android.accounts.Account;
+import android.content.Context;
+import android.util.Log;
+
+import com.github.mobile.R.string;
+import com.github.mobile.ui.ProgressDialogTask;
+import com.google.inject.Inject;
+
+/**
+ * Task to fork a repository
+ */
+public class ForkRepositoryTask extends ProgressDialogTask<Repository> {
+
+    private static final String TAG = "ForkRepositoryTask";
+
+    @Inject
+    private RepositoryService service;
+
+    private final IRepositoryIdProvider repo;
+
+    /**
+     * Create task for context and id provider
+     *
+     * @param context
+     * @param repo
+     */
+    public ForkRepositoryTask(Context context, IRepositoryIdProvider repo) {
+        super(context);
+
+        this.repo = repo;
+    }
+
+    /**
+     * Execute the task with a progress dialog displaying.
+     * <p>
+     * This method must be called from the main thread.
+     */
+    public void start() {
+        showIndeterminate(string.forking_repository);
+
+        execute();
+    }
+
+    @Override
+    protected Repository run(Account account) throws Exception {
+        service.forkRepository(repo);
+
+        return null;
+    }
+
+    @Override
+    protected void onException(Exception e) throws RuntimeException {
+        super.onException(e);
+
+        Log.d(TAG, "Exception forking repository", e);
+    }
+}
