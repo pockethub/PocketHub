34c028cb02bcd5706f8303b865725e5b263d2861
Artur Termenji <atermenji@gmail.com>
2013-02-05 20:33:08 +0200

    Implement search for a particular user

--- app/src/main/java/com/github/mobile/ui/search/SearchUserListFragment.java
+++ app/src/main/java/com/github/mobile/ui/search/SearchUserListFragment.java
@@ -15,31 +15,88 @@
  */
 package com.github.mobile.ui.search;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.content.Loader;
+import android.view.View;
+import android.widget.ListView;
 
 import com.github.kevinsawicki.wishlist.SingleTypeAdapter;
+import com.github.mobile.ThrowableLoader;
+import com.github.mobile.accounts.AccountUtils;
 import com.github.mobile.ui.ItemListFragment;
+import org.eclipse.egit.github.core.User;
+import com.github.mobile.R.string;
+import com.github.mobile.ui.user.UserListAdapter;
+import com.github.mobile.ui.user.UserViewActivity;
+import com.github.mobile.util.AvatarLoader;
+import com.google.inject.Inject;
 
+import org.eclipse.egit.github.core.service.UserService;
+
+import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.egit.github.core.User;
+import static android.app.SearchManager.QUERY;
 
-public class SearchUserListFragment extends
-        ItemListFragment<User> {
+/**
+ * Fragment to display the users from a search result
+ */
+public class SearchUserListFragment extends ItemListFragment<User> {
+
+    private String query;
+
+    @Inject
+    private UserService service;
+
+    @Inject
+    private AvatarLoader avatars;
 
     @Override
-    public Loader<List<User>> onCreateLoader(int id, Bundle args) {
-        return null;
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setEmptyText(string.no_people);
     }
 
     @Override
-    protected int getErrorMessage(Exception exception) {
-        return 0;
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        query = getStringExtra(QUERY);
+    }
+
+    @Override
+    public Loader<List<User>> onCreateLoader(int id, Bundle args) {
+        return new ThrowableLoader<List<User>>(getActivity(), items) {
+
+            @Override
+            public List<User> loadData() throws Exception {
+                List<User> users = new ArrayList<User>();
+                User user = service.getUser(query);
+                if (user != null)
+                    users.add(user);
+                return users;
+            }
+        };
     }
 
     @Override
     protected SingleTypeAdapter<User> createAdapter(List<User> items) {
-        return null;
+        User[] users = items.toArray(new User[items.size()]);
+        return new UserListAdapter(getActivity().getLayoutInflater(), users,
+                avatars);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        User user = (User) l.getItemAtPosition(position);
+        if (!AccountUtils.isUser(getActivity(), user))
+            startActivity(UserViewActivity.createIntent(user));
+    }
+
+    @Override
+    protected int getErrorMessage(Exception exception) {
+        return string.error_users_search;
     }
 }
