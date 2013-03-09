f7989066cd9b049bdb8feea65acb9d1b5509bcc4
Artur Termenji <atermenji@gmail.com>
2013-03-09 04:04:57 +0200

    UI for searching users
    Add SearchUserListAdapter, support for SearchUser class in
    AvatarLoader

--- app/src/main/java/com/github/mobile/ui/search/SearchUserListFragment.java
+++ app/src/main/java/com/github/mobile/ui/search/SearchUserListFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 GitHub Inc.
+ * Copyright 2013 GitHub Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,31 +24,29 @@ import android.widget.ListView;
 import com.github.kevinsawicki.wishlist.SingleTypeAdapter;
 import com.github.mobile.ThrowableLoader;
 import com.github.mobile.accounts.AccountUtils;
+import com.github.mobile.core.search.SearchUser;
+import com.github.mobile.core.search.SearchUserService;
+import com.github.mobile.core.user.RefreshUserTask;
 import com.github.mobile.ui.ItemListFragment;
 import org.eclipse.egit.github.core.User;
 import com.github.mobile.R.string;
-import com.github.mobile.ui.user.UserListAdapter;
 import com.github.mobile.ui.user.UserViewActivity;
 import com.github.mobile.util.AvatarLoader;
 import com.google.inject.Inject;
 
-import org.eclipse.egit.github.core.service.UserService;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static android.app.SearchManager.QUERY;
 
 /**
- * Fragment to display the users from a search result
+ * Fragment to display a list of {@link SearchUser} instances
  */
-public class SearchUserListFragment extends ItemListFragment<User> {
+public class SearchUserListFragment extends ItemListFragment<SearchUser> {
 
     private String query;
 
     @Inject
-    private UserService service;
+    private SearchUserService service;
 
     @Inject
     private AvatarLoader avatars;
@@ -75,36 +73,35 @@ public class SearchUserListFragment extends ItemListFragment<User> {
     }
 
     @Override
-    public Loader<List<User>> onCreateLoader(int id, Bundle args) {
-        return new ThrowableLoader<List<User>>(getActivity(), items) {
+    public Loader<List<SearchUser>> onCreateLoader(int id, Bundle args) {
+        return new ThrowableLoader<List<SearchUser>>(getActivity(), items) {
 
             @Override
-            public List<User> loadData() throws Exception {
-                items = Collections.emptyList();
-
-                User user = service.getUser(query);
-                if (user == null)
-                    return Collections.emptyList();
-
-                List<User> users = new ArrayList<User>();
-                users.add(user);
-                return users;
+            public List<SearchUser> loadData() throws Exception {
+                return service.searchUsers(query);
             }
         };
     }
 
     @Override
-    protected SingleTypeAdapter<User> createAdapter(List<User> items) {
-        User[] users = items.toArray(new User[items.size()]);
-        return new UserListAdapter(getActivity().getLayoutInflater(), users,
-                avatars);
+    protected SingleTypeAdapter<SearchUser> createAdapter(List<SearchUser> items) {
+        return new SearchUserListAdapter(getActivity(),
+            items.toArray(new SearchUser[items.size()]), avatars);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        User user = (User) l.getItemAtPosition(position);
-        if (!AccountUtils.isUser(getActivity(), user))
-            startActivity(UserViewActivity.createIntent(user));
+        final SearchUser result = (SearchUser) l.getItemAtPosition(position);
+        new RefreshUserTask(getActivity(), result.getLogin()) {
+
+            @Override
+            protected void onSuccess(User user) throws Exception {
+                super.onSuccess(user);
+
+                if (!AccountUtils.isUser(getActivity(), user))
+                    startActivity(UserViewActivity.createIntent(user));
+            }
+        }.execute();
     }
 
     @Override
