1827e098b202bca9f694a65bc8cb2d44d39f01cd
Artur Termenji <atermenji@gmail.com>
2013-10-29 19:06:47 +0200

    Implement two-factor authentication support

--- app/src/main/java/com/github/mobile/accounts/AccountAuthenticator.java
+++ app/src/main/java/com/github/mobile/accounts/AccountAuthenticator.java
@@ -90,7 +90,7 @@ class AccountAuthenticator extends AbstractAccountAuthenticator {
         return null;
     }
 
-    private boolean isValidAuthorization(final Authorization auth,
+    private static boolean isValidAuthorization(final Authorization auth,
             final List<String> requiredScopes) {
         if (auth == null)
             return false;
@@ -116,12 +116,12 @@ class AccountAuthenticator extends AbstractAccountAuthenticator {
      * Get existing authorization for this app
      *
      * @param service
-     * @param scopes
      * @return token or null if none found
      * @throws IOException
      */
-    private String getAuthorization(final OAuthService service,
-            final List<String> scopes) throws IOException {
+    public static String getAuthorization(final OAuthService service) throws IOException {
+        List<String> scopes = Arrays.asList("repo", "user", "gist");
+
         for (Authorization auth : service.getAuthorizations())
             if (isValidAuthorization(auth, scopes))
                 return auth.getToken();
@@ -132,12 +132,12 @@ class AccountAuthenticator extends AbstractAccountAuthenticator {
      * Create authorization for this app
      *
      * @param service
-     * @param scopes
      * @return created token
      * @throws IOException
      */
-    private String createAuthorization(final OAuthService service,
-            final List<String> scopes) throws IOException {
+    public static String createAuthorization(final OAuthService service) throws IOException {
+        List<String> scopes = Arrays.asList("repo", "user", "gist");
+
         Authorization auth = new Authorization();
         auth.setNote(APP_NOTE);
         auth.setNoteUrl(APP_NOTE_URL);
@@ -167,13 +167,12 @@ class AccountAuthenticator extends AbstractAccountAuthenticator {
         DefaultClient client = new DefaultClient();
         client.setCredentials(account.name, password);
         OAuthService service = new OAuthService(client);
-        List<String> scopes = Arrays.asList("repo", "user", "gist");
 
         String authToken;
         try {
-            authToken = getAuthorization(service, scopes);
+            authToken = getAuthorization(service);
             if (TextUtils.isEmpty(authToken))
-                authToken = createAuthorization(service, scopes);
+                authToken = createAuthorization(service);
         } catch (IOException e) {
             Log.e(TAG, "Authorization retrieval failed", e);
             throw new NetworkErrorException(e);
