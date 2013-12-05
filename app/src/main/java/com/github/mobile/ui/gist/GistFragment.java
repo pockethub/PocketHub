b45305e54761c138d86aacb2e9c81c811a3d1cea
Jonathan Maltz <maltz@yelp.com>
2013-12-05 22:27:43 +0800

    cleaned up useless log statements and made the enable wrapping and disable wrapping options work much cleaner

--- app/src/main/java/com/github/mobile/ui/gist/GistFragment.java
+++ app/src/main/java/com/github/mobile/ui/gist/GistFragment.java
@@ -27,7 +27,6 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
