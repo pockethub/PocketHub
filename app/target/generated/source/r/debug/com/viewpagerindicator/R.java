d6f9271afc190987863f22e55a16f3c71ddac116
Henrik Olsson <henrikolsson@outlook.com>
2014-08-07 15:21:30 +0200

    Removing unwanted

--- app/target/generated/source/r/debug/com/viewpagerindicator/R.java
+++ app/target/generated/source/r/debug/com/viewpagerindicator/R.java
@@ -84,13 +84,13 @@ public final class R {
 		public static final int default_title_indicator_top_padding = 0x7f090019;
 	}
 	public static final class drawable {
-		public static final int vpi__tab_indicator = 0x7f0200a9;
-		public static final int vpi__tab_selected_focused_holo = 0x7f0200aa;
-		public static final int vpi__tab_selected_holo = 0x7f0200ab;
-		public static final int vpi__tab_selected_pressed_holo = 0x7f0200ac;
-		public static final int vpi__tab_unselected_focused_holo = 0x7f0200ad;
-		public static final int vpi__tab_unselected_holo = 0x7f0200ae;
-		public static final int vpi__tab_unselected_pressed_holo = 0x7f0200af;
+		public static final int vpi__tab_indicator = 0x7f0200a8;
+		public static final int vpi__tab_selected_focused_holo = 0x7f0200a9;
+		public static final int vpi__tab_selected_holo = 0x7f0200aa;
+		public static final int vpi__tab_selected_pressed_holo = 0x7f0200ab;
+		public static final int vpi__tab_unselected_focused_holo = 0x7f0200ac;
+		public static final int vpi__tab_unselected_holo = 0x7f0200ad;
+		public static final int vpi__tab_unselected_pressed_holo = 0x7f0200ae;
 	}
 	public static final class id {
 		public static final int bottom = 0x7f08000d;
