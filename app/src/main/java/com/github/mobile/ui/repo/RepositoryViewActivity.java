669400a40050672e55ade0c0922f6befd0c77b24
Fadil Sutomo <fsutomo@gmail.com>
2014-12-16 17:54:04 +0700

    Revert "Revert "Add forking support""

--- app/src/main/java/com/github/mobile/ui/repo/RepositoryViewActivity.java
+++ app/src/main/java/com/github/mobile/ui/repo/RepositoryViewActivity.java
@@ -25,13 +25,13 @@ import static com.github.mobile.util.TypefaceUtils.ICON_COMMIT;
 import static com.github.mobile.util.TypefaceUtils.ICON_ISSUE_OPEN;
 import static com.github.mobile.util.TypefaceUtils.ICON_NEWS;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.ProgressBar;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.github.kevinsawicki.wishlist.ViewUtils;
 import com.github.mobile.Intents.Builder;
@@ -39,17 +39,19 @@ import com.github.mobile.R.id;
 import com.github.mobile.R.layout;
 import com.github.mobile.R.menu;
 import com.github.mobile.R.string;
+import com.github.mobile.core.repo.ForkRepositoryTask;
 import com.github.mobile.core.repo.RefreshRepositoryTask;
 import com.github.mobile.core.repo.RepositoryUtils;
 import com.github.mobile.core.repo.StarRepositoryTask;
 import com.github.mobile.core.repo.StarredRepositoryTask;
 import com.github.mobile.core.repo.UnstarRepositoryTask;
 import com.github.mobile.ui.TabPagerActivity;
+import com.github.mobile.ui.user.UriLauncherActivity;
 import com.github.mobile.ui.user.UserViewActivity;
 import com.github.mobile.util.AvatarLoader;
+import com.github.mobile.util.ShareUtils;
 import com.github.mobile.util.ToastUtils;
 import com.google.inject.Inject;
-import com.github.mobile.util.ShareUtils;
 
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.User;
@@ -57,8 +59,7 @@ import org.eclipse.egit.github.core.User;
 /**
  * Activity to view a repository
  */
-public class RepositoryViewActivity extends
-        TabPagerActivity<RepositoryPagerAdapter> {
+public class RepositoryViewActivity extends TabPagerActivity<RepositoryPagerAdapter> {
 
     /**
      * Create intent for this activity
@@ -96,8 +97,7 @@ public class RepositoryViewActivity extends
         actionBar.setSubtitle(owner.getLogin());
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        if (owner.getAvatarUrl() != null
-                && RepositoryUtils.isComplete(repository))
+        if (owner.getAvatarUrl() != null && RepositoryUtils.isComplete(repository))
             configurePager();
         else {
             avatars.bind(getSupportActionBar(), owner);
@@ -106,8 +106,7 @@ public class RepositoryViewActivity extends
             new RefreshRepositoryTask(this, repository) {
 
                 @Override
-                protected void onSuccess(Repository fullRepository)
-                        throws Exception {
+                protected void onSuccess(Repository fullRepository) throws Exception {
                     super.onSuccess(fullRepository);
 
                     repository = fullRepository;
@@ -118,8 +117,7 @@ public class RepositoryViewActivity extends
                 protected void onException(Exception e) throws RuntimeException {
                     super.onException(e);
 
-                    ToastUtils.show(RepositoryViewActivity.this,
-                            string.error_repo_load);
+                    ToastUtils.show(RepositoryViewActivity.this, string.error_repo_load);
                     ViewUtils.setGone(loadingBar, true);
                 }
             }.execute();
@@ -155,8 +153,7 @@ public class RepositoryViewActivity extends
 
     @Override
     public void onBackPressed() {
-        if (adapter == null || pager.getCurrentItem() != ITEM_CODE
-                || !adapter.onBackPressed())
+        if (adapter == null || pager.getCurrentItem() != ITEM_CODE || !adapter.onBackPressed())
             super.onBackPressed();
     }
 
@@ -171,33 +168,35 @@ public class RepositoryViewActivity extends
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case id.m_star:
-            starRepository();
-            return true;
-        case id.m_share:
-            shareRepository();
-            return true;
-        case id.m_refresh:
-            checkStarredRepositoryStatus();
-            return super.onOptionsItemSelected(item);
-        case id.m_contributors:
-            startActivity(RepositoryContributorsActivity.createIntent(repository));
-            return true;
-        case android.R.id.home:
-            finish();
-            Intent intent = UserViewActivity.createIntent(repository.getOwner());
-            intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_SINGLE_TOP);
-            startActivity(intent);
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
+            case id.m_star:
+                starRepository();
+                return true;
+            case id.m_fork:
+                forkRepository();
+                return true;
+            case id.m_contributors:
+                startActivity(RepositoryContributorsActivity.createIntent(repository));
+                return true;
+            case id.m_share:
+                shareRepository();
+                return true;
+            case id.m_refresh:
+                checkStarredRepositoryStatus();
+                return super.onOptionsItemSelected(item);
+            case android.R.id.home:
+                finish();
+                Intent intent = UserViewActivity.createIntent(repository.getOwner());
+                intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
     @Override
     public void onDialogResult(int requestCode, int resultCode, Bundle arguments) {
-        adapter.onDialogResult(pager.getCurrentItem(), requestCode, resultCode,
-                arguments);
+        adapter.onDialogResult(pager.getCurrentItem(), requestCode, resultCode, arguments);
     }
 
     @Override
@@ -213,16 +212,16 @@ public class RepositoryViewActivity extends
     @Override
     protected String getIcon(int position) {
         switch (position) {
-        case 0:
-            return ICON_NEWS;
-        case 1:
-            return ICON_CODE;
-        case 2:
-            return ICON_COMMIT;
-        case 3:
-            return ICON_ISSUE_OPEN;
-        default:
-            return super.getIcon(position);
+            case 0:
+                return ICON_NEWS;
+            case 1:
+                return ICON_CODE;
+            case 2:
+                return ICON_COMMIT;
+            case 3:
+                return ICON_ISSUE_OPEN;
+            default:
+                return super.getIcon(position);
         }
     }
 
@@ -242,8 +241,7 @@ public class RepositoryViewActivity extends
                 protected void onException(Exception e) throws RuntimeException {
                     super.onException(e);
 
-                    ToastUtils.show(RepositoryViewActivity.this,
-                            string.error_unstarring_repository);
+                    ToastUtils.show(RepositoryViewActivity.this, string.error_unstarring_repository);
                 }
             }.start();
         else
@@ -261,8 +259,7 @@ public class RepositoryViewActivity extends
                 protected void onException(Exception e) throws RuntimeException {
                     super.onException(e);
 
-                    ToastUtils.show(RepositoryViewActivity.this,
-                            string.error_starring_repository);
+                    ToastUtils.show(RepositoryViewActivity.this, string.error_starring_repository);
                 }
             }.start();
     }
@@ -285,9 +282,31 @@ public class RepositoryViewActivity extends
     private void shareRepository() {
         String repoUrl = repository.getHtmlUrl();
         if (TextUtils.isEmpty(repoUrl))
-          repoUrl = "https://github.com/" + repository.generateId();
-        Intent sharingIntent = ShareUtils.create(repository.generateId(),
-                                                 repoUrl);
+            repoUrl = "https://github.com/" + repository.generateId();
+        Intent sharingIntent = ShareUtils.create(repository.generateId(), repoUrl);
         startActivity(sharingIntent);
+    }
+
+    private void forkRepository() {
+        new ForkRepositoryTask(this, repository) {
+
+            @Override
+            protected void onSuccess(Repository e) throws Exception {
+                super.onSuccess(e);
+
+                if (e != null) {
+                    UriLauncherActivity.launchUri(getContext(), Uri.parse(e.getHtmlUrl()));
+                } else {
+                    ToastUtils.show(RepositoryViewActivity.this, string.error_forking_repository);
+                }
+            }
+
+            @Override
+            protected void onException(Exception e) throws RuntimeException {
+                super.onException(e);
+
+                ToastUtils.show(RepositoryViewActivity.this, string.error_forking_repository);
+            }
+        }.start();
     }
 }
