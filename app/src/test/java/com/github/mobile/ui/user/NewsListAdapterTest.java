446dea7c34b9f05d4b1085a7cf9dc81cc87641da
Ben Wu <wubin28@gmail.com>
2015-03-20 09:19:31 -0400

    Made Android Studio support unit testing.

---
+++ app/src/test/java/com/github/mobile/ui/user/NewsListAdapterTest.java
@@ -0,0 +1,13 @@
+package com.github.mobile.ui.user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NewsListAdapterTest {
+
+    @Test
+    public void testUpdate() throws Exception {
+        assertEquals(1, 1);
+    }
+}
