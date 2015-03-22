763efec2dbf2adf3df945ddcf072c5a8da6aee88
Ben Wu <wubin28@gmail.com>
2015-03-22 06:21:34 -0400

    Wrote Arrange of the test. Need to eliminate static method calls NewsListAdapter.formatXxx() in IconAndViewTextManager.setIconAndFormatStyledText() and then mock it in testing.

--- app/src/test/java/com/github/mobile/ui/user/IconAndViewTextManagerTest.java
+++ app/src/test/java/com/github/mobile/ui/user/IconAndViewTextManagerTest.java
@@ -2,6 +2,7 @@ package com.github.mobile.ui.user;
 
 import com.github.mobile.util.TypefaceUtils;
 
+import org.eclipse.egit.github.core.event.Event;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,9 +12,12 @@ public class IconAndViewTextManagerTest {
     @Test
     public void when_event_type_is_commit_comment_then_icon_should_be_comment() throws Exception {
         // Arrange
+        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+        Event event = new Event();
+        event.setType(Event.TYPE_COMMIT_COMMENT);
 
         // Act
-        String icon = iconAndViewTextManager.setIconAndFormatStyledText(event, main, details);
+        String icon = iconAndViewTextManager.setIconAndFormatStyledText(event, null, null);
 
         // Assert
         assertEquals(TypefaceUtils.ICON_COMMENT, icon);
