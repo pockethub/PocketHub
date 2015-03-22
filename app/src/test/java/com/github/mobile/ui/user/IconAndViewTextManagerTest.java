140f890ece25dfd87e11c22479b0be8f7b96995c
Ben Wu <wubin28@gmail.com>
2015-03-22 13:53:03 -0400

    Replaced subclass-and-override testing class with mockito spy.

--- app/src/test/java/com/github/mobile/ui/user/IconAndViewTextManagerTest.java
+++ app/src/test/java/com/github/mobile/ui/user/IconAndViewTextManagerTest.java
@@ -6,18 +6,23 @@ import org.eclipse.egit.github.core.event.Event;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
 
 public class IconAndViewTextManagerTest {
 
     @Test
     public void when_event_type_is_commit_comment_then_icon_should_be_comment() throws Exception {
         // Arrange
-        IconAndViewTextManager iconAndViewTextManager = new TestingIconAndViewTextManager(null);
         Event event = new Event();
         event.setType(Event.TYPE_COMMIT_COMMENT);
 
+        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+        IconAndViewTextManager spyIconAndViewTextManager = spy(iconAndViewTextManager);
+        doNothing().when(spyIconAndViewTextManager).formatCommitComment(event, null, null);
+
         // Act
-        String icon = iconAndViewTextManager.setIconAndFormatStyledText(event, null, null);
+        String icon = spyIconAndViewTextManager.setIconAndFormatStyledText(event, null, null);
 
         // Assert
         assertEquals(TypefaceUtils.ICON_COMMENT, icon);
