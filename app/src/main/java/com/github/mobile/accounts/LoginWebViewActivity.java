a7401e5091c06c68fae499ea1972b40143c66fa9
Henrik <henrikolsson@outlook.com>
2015-07-11 14:20:36 +0200

    Added log message if build value is not found Moved from browser to WebView for authentication Added disclaimer for third-party restrictions Updated two libs (It changes nothing)

---
+++ app/src/main/java/com/github/mobile/accounts/LoginWebViewActivity.java
@@ -0,0 +1,36 @@
+package com.github.mobile.accounts;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebViewClient;
+
+import com.github.mobile.R;
+import com.github.mobile.ui.WebView;
+
+public class LoginWebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.loadUrl(getIntent().getStringExtra(LoginActivity.INTENT_EXTRA_URL));
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(android.webkit.WebView view, String url) {
+                Uri uri = Uri.parse(url);
+                if (uri.getScheme().equals(getString(R.string.github_oauth_scheme))) {
+                    Intent data = new Intent();
+                    data.setData(uri);
+                    setResult(RESULT_OK, data);
+                    finish();
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+        setContentView(webView);
+    }
+}
