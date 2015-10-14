aeade9dd26581cb470598de405c2cc3be97088d7
Lars Grefer <github@larsgrefer.de>
2015-10-14 22:51:09 +0200

    add SettingsActivity

---
+++ app/src/main/java/com/github/pockethub/ui/SettingsActivity.java
@@ -0,0 +1,9 @@
+package com.github.pockethub.ui;
+
+import io.freefair.android.appcompatPreference.AppCompatPreferenceActivity;
+
+/**
+ * Created by larsgrefer on 14.10.15.
+ */
+public class SettingsActivity extends AppCompatPreferenceActivity {
+}
