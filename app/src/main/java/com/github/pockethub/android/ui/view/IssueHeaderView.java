2ff9b15fded73c19cc4c8051bad1c4274abf1f3e
StefMa <StefMaDev@outlook.com>
2016-10-01 17:57:13 +0200

    Styled issue header

--- app/src/main/java/com/github/pockethub/android/ui/view/IssueHeaderView.java
+++ app/src/main/java/com/github/pockethub/android/ui/view/IssueHeaderView.java
@@ -4,18 +4,13 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.alorma.github.sdk.bean.dto.response.Issue;
 import com.github.pockethub.android.R;
 import com.github.pockethub.android.ui.StyledText;
-import com.github.pockethub.android.util.AvatarLoader;
 import com.github.pockethub.android.util.TimeUtils;
-import com.google.inject.Inject;
-
-import roboguice.RoboGuice;
 
 public class IssueHeaderView extends LinearLayout {
 
@@ -25,11 +20,7 @@ public class IssueHeaderView extends LinearLayout {
 
     private TextView createdDateText;
 
-    private ImageView creatorAvatar;
-
-    @Inject
-    @SuppressWarnings("unused")
-    private AvatarLoader avatars;
+    private TextView commentCount;
 
     public IssueHeaderView(final Context context) {
         this(context, null);
@@ -55,15 +46,16 @@ public class IssueHeaderView extends LinearLayout {
     private void init() {
         setOrientation(VERTICAL);
 
-        if (!isInEditMode()) {
-            RoboGuice.getInjector(getContext()).injectMembers(this);
-        }
-
         inflate(getContext(), R.layout.view_issue_header, this);
         titleText = (TextView) findViewById(R.id.tv_issue_title);
         authorText = (TextView) findViewById(R.id.tv_issue_author);
         createdDateText = (TextView) findViewById(R.id.tv_issue_creation_date);
-        creatorAvatar = (ImageView) findViewById(R.id.iv_avatar);
+        commentCount = (TextView) findViewById(R.id.tv_issue_comments_count);
+
+        final TextView authorDateSeparator = (TextView) findViewById(R.id.tv_issue_author_date_separator);
+        authorDateSeparator.setText(" ");
+        final TextView dateCommentsSeparator = (TextView) findViewById(R.id.tv_issue_date_comments_separator);
+        dateCommentsSeparator.setText(" · ");
     }
 
     public void updateHeader(final Issue issue) {
@@ -72,7 +64,9 @@ public class IssueHeaderView extends LinearLayout {
         createdDateText.setText(new StyledText().append(
                 getContext().getString(R.string.prefix_opened)).append(TimeUtils.stringToDate
                 (issue.created_at)));
-        avatars.bind(creatorAvatar, issue.user);
+        final int comments = issue.comments;
+        final String commentString = getContext().getString(R.string.comments);
+        commentCount.setText(String.format("%1$s " + commentString, comments));
     }
 
 }
