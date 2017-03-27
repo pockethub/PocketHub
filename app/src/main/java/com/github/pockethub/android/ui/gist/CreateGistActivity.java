b9e32ec35fb9689c6ec20b0d0c90b5d999d6eec0
Hunk Deng <niknow@live.cn>
2017-03-27 21:47:24 +0800

    Finish screen when click home button in Create Gist Screen.

--- app/src/main/java/com/github/pockethub/android/ui/gist/CreateGistActivity.java
+++ app/src/main/java/com/github/pockethub/android/ui/gist/CreateGistActivity.java
@@ -150,6 +150,9 @@ public class CreateGistActivity extends BaseActivity {
             case R.id.create_gist:
                 createGist();
                 return true;
+            case android.R.id.home:
+                finish();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
