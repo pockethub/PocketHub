a06e15be3563adc804d8bde199ef41d378de74df
devopsworksio <neil@devopsworks.io>
2017-09-15 15:54:10 +0100

    Added pipeline stuffAdded pipeline stuff

--- scripts/jenkins/lib/git-status.groovy
+++ scripts/jenkins/lib/git-status.groovy
@@ -2,20 +2,23 @@ def gitStatusEnabled(String context, Closure buildStep, Closure postBuildStep) {
     reportGitStatus(context, "Running $context...", "pending")
     try {
         buildStep.call()
+        println (">>> Build Step $context passed! <<<")
         reportGitStatus(context, "$context passed!", "success")
+
         currentBuild.result = 'SUCCESS'
     } catch (err) {
         reportGitStatus(context, "$context failed!", "failure")
         common.notifyJira("Build Failed! ${err} ..." , "${env.JIRA_ISSUE}")
         currentBuild.result = 'FAILURE'
         // this will stop the build and mark it as failed
-        error ("### Build failed in ${err.message}...! ###")
+        error (">>> Build failed in ${err.message}...! <<<")
     } finally {
         postBuildStep.call()
     }
 }
 
 void reportGitStatus(String context, String description, String status) {
+
     withCredentials([string(credentialsId: 'JENKINS_PERSONAL_ACCESS_TOKEN', variable: 'JENKINS_PERSONAL_ACCESS_TOKEN')]) {
         sh 'env | grep JENKINS'
         try {
