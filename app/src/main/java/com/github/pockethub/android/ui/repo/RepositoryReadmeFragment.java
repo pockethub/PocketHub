367295a719f9f7e74d7b33703c8a116fe9a42e14
Henrik Olsson <saviours-kid97@hotmail.se>
2019-01-06 14:24:06 +0100

    Fixed issues with Readme style and code

--- app/src/main/java/com/github/pockethub/android/ui/repo/RepositoryReadmeFragment.java
+++ app/src/main/java/com/github/pockethub/android/ui/repo/RepositoryReadmeFragment.java
@@ -24,7 +24,7 @@ public class RepositoryReadmeFragment extends BaseFragment {
 
     private static final String PAGE_START = "<!DOCTYPE html><html lang=\"en\"> <head> <title></title>" +
             "<meta charset=\"UTF-8\"> " +
-            "<meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;\"/>" +
+            "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\"/>" +
             "<script src=\"intercept.js\"></script>" +
             "<link href=\"github.css\" rel=\"stylesheet\"> </head> <body>";
 
