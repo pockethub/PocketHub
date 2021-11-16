2398d90b44a965a4d7fc1d2286ad1ed5a07a455c
Nahian98 <kanon1707030@stud.kuet.ac.bd>
2021-11-17 00:55:35 +0600

    Another class Added

--- class1.java
+++ class1.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.myapplication", appContext.getPackageName());
+	assertEquals("com.example.myapplication", appcontext.getPackagename());
     }
 }
